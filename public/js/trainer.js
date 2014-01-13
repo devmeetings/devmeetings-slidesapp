38d29f7d1a304858ad9c6ac40e1b4cdb0199fa69
Tomusdrw <tomusdrw@gmail.com>
2014-01-13 17:49:41 +0100

    Solutions are now visible in trainers mode. Some small visual improvemenets.

--- public/js/trainer.js
+++ public/js/trainer.js
@@ -3,19 +3,26 @@
 
     var parentFrame = null;
 
-    var SolutionButton = (function() {
+    var Solution = (function() {
+        var $solutionSpace = $('.task-solution');
+        var $notesSpace = $('.nextslide-space');
+
         var $el = $('.task-solution-btn');
+        var $solution = $('iframe.task-solution-frame');
         var taskData = null;
 
-        var Btn = {
-            show: function(show) {
-                $el.toggleClass('hidden', !show);
+        var Solution = {
+            showButton: function(show) {
+                $solutionSpace.toggleClass('hidden', !show);
+                $notesSpace.toggleClass('col-md-6', !! show).toggleClass('col-md-12', !show);
             },
             reset: function(newTaskData) {
                 taskData = newTaskData;
-                $el.text('Show task solution');
+                if (taskData && taskData.solution) {
+                    $solution[0].src = '/slide?slide=' + encodeURIComponent(JSON.stringify(taskData.solution));
+                }
             },
-            click: function() {
+            display: function() {
                 parentFrame.postMessage({
                     type: 'tasksolution',
                     solution: taskData.solution
@@ -23,16 +30,16 @@
             }
         };
 
-        $el.on('click', Btn.click.bind(Btn));
+        $el.on('click', Solution.display.bind(Solution));
 
-        return Btn;
+        return Solution;
     }());
 
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/javascript");
 
-    var iframe = $('iframe')[0];
+    var iframe = $('iframe.next-slide')[0];
 
     window.addEventListener('message', function(ev) {
         var data = ev.data;
@@ -42,8 +49,8 @@
         parentFrame = ev.source;
 
         editor.setValue(data.currentSlide.notes);
-        SolutionButton.show(data.currentSlide.task);
-        SolutionButton.reset(data.currentSlide.task);
+        Solution.showButton(data.currentSlide.task);
+        Solution.reset(data.currentSlide.task);
 
         iframe.src = '/slide?slide=' + encodeURIComponent(JSON.stringify(data.nextSlide));
     });
