154e9807fa408bb16cd2ce4f207446e30c575461
Tomusdrw <tomusdrw@gmail.com>
2014-03-19 17:07:04 +0100

    Cleanup.

--- platform/mongo.js
+++
@@ -1,9 +0,0 @@
-var Client = require('mongodb').MongoClient;
-
-
-Client.connect('mongodb://localhost:27017/konkurs', function(err, db) {
-    if (err) throw err;
-
-    exports.code = db.collection('code');
-    exports.submit = db.collection('submit');
-});
