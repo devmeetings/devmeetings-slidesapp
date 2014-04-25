17e1a484afe550a5b28490c0a057f1fef80b40ff
Tomusdrw <tomusdrw@gmail.com>
2014-04-25 20:06:34 +0200

    Fixed issue with windows paths for connect-static-jade.

---
+++ platform/microtask.js
@@ -0,0 +1,39 @@
+﻿var sys = require("sys");
+var esprima = require('esprima');
+var jqgram = require('jqgram').jqgram;
+
+var code1 = "var i=0; if(i) { };";
+var code2 = "var z=5;";
+
+var tree1 = esprima.parse(code1);
+var tree2 = esprima.parse(code2);
+
+Object.prototype.ts = function() {
+    return JSON.stringify(this, null, '  ');
+}
+
+sys.puts(tree1.ts());
+sys.puts(tree2.ts());
+
+function lfn(node) {
+    return node.type;
+}
+
+function cfn(treename) {
+    return function (node) {
+        sys.puts(treename + " node: " + node.ts());
+        //sys.puts("node: " + node.);
+        if (node.type == 'VariableDeclaration')
+            return node.declarations;
+        else if (node.type == 'Program')
+            return node.body;
+        else if (node.type == 'IfStatement') {
+        }
+        else return null;
+    }
+}
+
+jqgram.distance({ root: tree1, lfn: lfn, cfn: cfn("tree1") }, { root: tree2, lfn: lfn, cfn: cfn("tree2") }, { p: 2, q: 3, depth: 10 },
+    function (result) {
+        sys.puts(result.distance);
+    });
