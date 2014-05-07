6d111114142880198ac7cc3b0c65ac196ae6192e
Tomusdrw <tomusdrw@gmail.com>
2014-05-07 09:09:12 +0200

    Plugins support & auto-loading on backend side.

--- platform/app/plugins/slide-chat/slide-chat.js
+++ platform/app/plugins/slide-chat/slide-chat.js
@@ -1,8 +1,8 @@
-var CommentModel = require('../models/comment');
+var CommentModel = require('./comment');
 
+exports.socketInit = function(log, socket) {
 
-exports.joinChat = function(socket) {
-    return function(data, res) {
+    var joinChat = function(data, res) {
         if (socket.room) {
             socket.leave(socket.room);
         }
@@ -21,10 +21,8 @@ exports.joinChat = function(socket) {
             res(comments);
         });
     };
-};
 
-exports.sendChatMsg = function(socket) {
-    return function(data, res) {
+    var sendChatMsg = function(data, res) {
         var d = new CommentModel(data);
         d.save(function(err, comment) {
             if (err) {
@@ -34,4 +32,8 @@ exports.sendChatMsg = function(socket) {
             socket.broadcast.to(socket.room).emit('chat.msg.send', data);
         });
     };
+
+
+    socket.on('chat.join', joinChat);
+    socket.on('chat.msg.send', sendChatMsg);
 };
--- platform/config/sockets.js
+++ platform/config/sockets.js
@@ -1,7 +1,7 @@
-var log = function(socket) {
+var log = function(socket, pluginName) {
     return function() {
         var args = [].slice.call(arguments);
-        args.unshift("[" + socket.id + "] ");
+        args.unshift("[" + socket.id + "][" + pluginName + "] ");
         console.log.apply(console, args);
     };
 };
@@ -10,16 +10,37 @@ var ctrl = function(ctrlName) {
     return require('../app/controllers/' + ctrlName);
 };
 
+
+var fs = require('promised-io/fs');
+var pluginsPath = 'app/plugins/';
+
+var plugins = fs.readdir(pluginsPath).then(function(files) {
+    return files.map(function(pluginName) {
+        try {
+            var mod = require("../" + pluginsPath + pluginName);
+            mod.name = pluginName;
+            return mod;
+        } catch (e) {
+            console.warn("Cannot load plugin " + pluginName, e);
+            return null;
+        }
+    }).filter(function(plugin) {
+        return plugin && plugin.socketInit;
+    });
+});
+
 module.exports = function(io) {
     io.on('connection', function(socket) {
-        var id = socket.id;
-        var l = log(socket);
-
+        var l = log(socket, "main");
         l("New client connected");
 
-        var comments = ctrl('comments');
-        socket.on('chat.join', comments.joinChat(socket));
-        socket.on('chat.msg.send', comments.sendChatMsg(socket));
+        plugins.then(function(pluginsList) {
 
+            pluginsList.forEach(function(plugin) {
+
+                plugin.socketInit(log(socket, plugin.name), socket);
+
+            });
+        });
     });
 };
