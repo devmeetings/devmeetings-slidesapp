38a18505d1b664ef55a360c66fd890b7753b837b
Tomusdrw <tomusdrw@gmail.com>
2014-05-13 15:13:58 +0200

    Removed presetntation-layout-std. Using plugins-loader instead. Added editing of slides.

---
+++ platform/public/plugins/deck-navbar/deck-navbar.js
@@ -0,0 +1,45 @@
+define(['module', '_', 'slider/slider.plugins'], function(module, _, sliderPlugins) {
+
+    var path = sliderPlugins.extractPath(module);
+
+    sliderPlugins.registerPlugin('deck', 'slides', 'deck-navbar', 1).directive('deckNavbar', [
+
+        function() {
+
+            return {
+                restrict: 'E',
+                scope: {
+                    slides: '=data',
+                    deck: '=context'
+                },
+                templateUrl: path + '/deck-navbar.html',
+
+                controller: ['$rootScope', '$scope', '$location', '$http',
+                    function($rootScope, $scope, $location, $http) {
+                        $rootScope.title = $scope.deck.title;
+
+                        $scope.changeSlide = function() {
+                            var previousSlide = $scope.activeSlide;
+                            $scope.activeSlide = $location.url().substr(1);
+                            sliderPlugins.trigger('slide.current.change', $scope.activeSlide, previousSlide);
+                        };
+
+                        $scope.changeSlide();
+                        $scope.$on('$locationChangeSuccess', $scope.changeSlide);
+
+                        $scope.addSlide = function() {
+                            // Update deck
+                            $scope.deck.slides.push({
+                                id: 'empty',
+                                name: 'New slide'
+                            });
+                            // TODO [ToDr] TEMP hack
+                            $http.put('/api/decks/' + slides, $scope.deck);
+                        };
+                    }
+                ]
+            };
+        }
+    ]);
+
+});
---
+++ platform/public/plugins/deck-slides/deck-slides.js
@@ -0,0 +1,33 @@
+define(['module', '_', 'slider/slider.plugins'], function(module, _, sliderPlugins) {
+
+    var path = sliderPlugins.extractPath(module);
+
+    sliderPlugins.registerPlugin('deck', 'slides', 'deck-slides').directive('deckSlides', [
+
+        function() {
+
+            return {
+                restrict: 'E',
+                scope: {
+                    slides: '=data',
+                    deck: '=context'
+                },
+                templateUrl: path + '/deck-slides.html',
+
+                controller: ['$rootScope', '$scope', '$location',
+                    function($rootScope, $scope, $location) {
+
+                        sliderPlugins.listen($scope, 'slide.current.change', function(activeSlide) {
+                            var absUrl = $location.absUrl();
+                            var len = absUrl.indexOf("?") > -1 ? absUrl.indexOf("?") : absUrl.indexOf("#");
+                            var path = absUrl.substr(0, len);
+                            $scope.slideSource = path + '/slide-' + activeSlide + ($rootScope.editMode ? '?edit=true' : '');
+                        });
+
+                    }
+                ]
+            };
+        }
+    ]);
+
+});
