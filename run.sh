75673636b59861decd6979fd732971791692e2d2
Tomusdrw <tomusdrw@gmail.com>
2014-05-21 11:20:20 +0200

    --allow-root for bower. Auto opening in browser.

--- run.sh
+++ run.sh
@@ -18,8 +18,14 @@ docker run -d --name mongo -t xplatform/mongo
 docker run -d --name rabbit -p :5672 -p :15672 -e RABBITMQ_PASS="kbAc4kRS" -t xplatform/rabbitmq 
 docker run -d --link rabbit:rabbit --name exec-node -t xplatform/executors-node
 
+
+docker stop xplatform
+docker rm xplatform
 docker run -p 3000 --link mongo:mongo --link rabbit:rabbit --name xplatform -e MONGO_HOST="mongo" -t xplatform/platform
 
 #docker build -t devmeetings/xplatform-basic .
 #docker run -v $cwd/platform:/platform -p 3000 -t devmeetings/xplatform-basic
 docker ps
+
+PORT=$(docker ps | grep xplatform/platform | sed 's/.*\(49[0-9]\{3\}\).*/\1/')
+open http://localhost:${PORT}
