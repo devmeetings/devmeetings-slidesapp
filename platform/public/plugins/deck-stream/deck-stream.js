56cce7238829acfa3eca8a373436fd5bc562a822
Tomusdrw <tomusdrw@gmail.com>
2014-05-28 18:39:42 +0200

    Deck-stream plugin.

---
+++ platform/public/plugins/deck-stream/deck-stream.js
@@ -0,0 +1,37 @@
+define(['module', 'slider/slider.plugins'], function(module, sliderPlugins) {
+    var path = sliderPlugins.extractPath(module);
+
+    sliderPlugins.registerPlugin('deck', '*', 'deck-stream').directive('deckStream', [
+        'Sockets',
+        function(Sockets) {
+            return {
+                restrict: 'E',
+                scope: {
+                    data: '=data',
+                    slide: '=context'
+                },
+                templateUrl: path + '/deck-stream.html',
+                link: function(scope, element) {
+                    scope.statuses = [];
+
+                    scope.setStatus = function(status) {
+                        Sockets.emit('stream.status', status);
+                        scope.statuses.push({
+                            date: new Date(),
+                            status: status
+                        });
+                    };
+
+                    Sockets.on('stream.status', function(status) {
+                        scope.statuses.push({
+                            date: new Date(),
+                            status: status
+                        });
+                    });
+
+                }
+            };
+        }
+    ]);
+
+});
