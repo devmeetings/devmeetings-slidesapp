ec89bd407988bdfc971850b2655e818e6ff004ae
Tomusdrw <tomusdrw@gmail.com>
2014-05-28 09:14:51 +0200

    Fixing complexity issues. Removed PWYW plugins.

--- platform/public/plugins/slide-speedDating/slide-speedDating.js
+++ platform/public/plugins/slide-speedDating/slide-speedDating.js
@@ -23,82 +23,91 @@ define(['module', '_', 'slider/slider.plugins', 'howler'], function(module, _, s
                 },
                 templateUrl: path + '/slide-speedDating.html',
                 link: function(scope, element) {
-                  _.defaults(scope.speedDating, {
-                    time: 15,
-                    perPerson: 40
-                  });
-                  var toInt = function(x) {
-                    var i = parseInt(x, 10);
-                    return isNaN(i) ? 0 : i;
-                  };
-                  var updateInteractions = function() {
-                    scope.interactions = Math.floor(toInt(scope.speedDating.time) * 60 / (toInt(scope.speedDating.perPerson) + 5)) / 2;
-                    scope.session.left = scope.interactions;
-                  };
-                  scope.$watch('speedDating', updateInteractions, true);
-
-
-                  scope.session = {
-                    running: false,
-                    timeLeft: 5
-                  };
-
-                  scope.testPlay = function(what) {
-                    sounds.play(what);
-                  };
-
-                  scope.startDating = function() {
-                    var session = {
-                      running: true,
-                      type: 0,
-                      left: scope.interactions,
-                      endDate: new Date()
-                    };
-
-                    var updateTimeLeft = function() {
-                      session.timeLeft = (session.endDate.getTime() - new Date().getTime()) / 1000;
-                      return session.timeLeft <= 0;
-                    };
-
-
-                    var timer = function (self, time, other, starting) {
-                      if (starting) {
-                        session.endDate = new Date(new Date().getTime() + time * 1000);
-                      }
-                      var isFinished = updateTimeLeft();
-                      if (isFinished) {
-                        session.type = (session.type + 1) % 4;
-                        if (session.type === 0) {
-                          session.left--;
-                        }
-                        var toPlay = ['newPerson', 'voiceSwap'];
-                        sounds.play(toPlay[session.type / 2]);
-
-                        if (session.left) {
-                          other();
-                        } else {
-                          session.running = false;
-                        }
-                      } else {
-                        $timeout(self(), 200);
-                      }
-                    };
-
-                    var change = timer.bind(null, function() {
-                      return change;
-                    }, 5, function() {
-                      talking(true);
+                    _.defaults(scope.speedDating, {
+                        time: 15,
+                        perPerson: 40
                     });
-                    var talking = timer.bind(null, function() {
-                      return talking;
-                    }, toInt(scope.speedDating.perPerson), function(){
-                      change(true);
-                    });
+                    var toInt = function(x) {
+                        var i = parseInt(x, 10);
+                        return isNaN(i) ? 0 : i;
+                    };
+                    var updateInteractions = function() {
+                        scope.interactions = Math.floor(toInt(scope.speedDating.time) * 60 / (toInt(scope.speedDating.perPerson) + 5)) / 2;
+                        scope.session.left = scope.interactions;
+                    };
+                    scope.$watch('speedDating', updateInteractions, true);
 
-                    scope.session = session;
-                    sounds.play('newPerson');
-                    change(true);
-                  };
+
+                    scope.session = {
+                        running: false,
+                        timeLeft: 5
+                    };
+
+                    scope.testPlay = function(what) {
+                        sounds.play(what);
+                    };
+
+                    scope.startDating = function() {
+                        var session = {
+                            running: true,
+                            type: 0,
+                            left: scope.interactions,
+                            endDate: new Date()
+                        };
+
+                        var updateTimeLeft = function() {
+                            session.timeLeft = (session.endDate.getTime() - new Date().getTime()) / 1000;
+                            return session.timeLeft <= 0;
+                        };
+
+                        var updateSession = function() {
+                            session.type = (session.type + 1) % 4;
+                            if (session.type === 0) {
+                                session.left--;
+                            }
+                        };
+                        var playSound = function() {
+                            var toPlay = ['newPerson', 'voiceSwap'];
+                            sounds.play(toPlay[session.type / 2]);
+                        };
+
+
+                        var timer = function(self, time, other, starting) {
+                            if (starting) {
+                                session.endDate = new Date(new Date().getTime() + time * 1000);
+                            }
+
+                            var isFinished = updateTimeLeft();
+                            if (isFinished) {
+
+                                updateSession();
+                                playSound();
+                                // ending
+                                if (session.left) {
+                                    other();
+                                } else {
+                                    session.running = false;
+                                }
+                            } else {
+                                $timeout(self(), 200);
+                            }
+                        };
+
+                        var change = timer.bind(null, function() {
+                            return change;
+                        }, 5, function() {
+                            talking(true);
+                        });
+                        var talking = timer.bind(null, function() {
+                            return talking;
+                        }, toInt(scope.speedDating.perPerson), function() {
+                            change(true);
+                        });
+
+                        scope.session = session;
+                        sounds.play('newPerson');
+                        change(true);
+                    };
                 }
             };
         }
