0c43036781ab972960ea1998032b09370dd99f99
Tomusdrw <tomusdrw@gmail.com>
2014-06-02 17:22:56 +0200

    Changed port of depployment app.

--- deployment/config.js
+++ deployment/config.js
@@ -4,7 +4,7 @@ module.exports = {
 
     appName: 'XPlatform.org',
     logsPath: xplatformDir + 'deployment/logs/',
-    port: process.env.PORT || 3000,
+    port: 9000,
 
     environments: {
         "xplatform-prod": {
