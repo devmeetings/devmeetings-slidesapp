0f11b47bd9bf197277024cd0f93c82ccf25f5add
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-06-02 18:24:49 +0200

    add new slides

--- platform/public/plugins/deck-navbar/deck-navbar.js
+++ platform/public/plugins/deck-navbar/deck-navbar.js
@@ -1,28 +1,39 @@
-define(['module', '_', 'slider/slider.plugins', 'services/CurrentSlideManager'], function(module, _, sliderPlugins, CurrentSlideManager) {
+define(['module', '_', 'slider/slider.plugins', 'services/CurrentSlideManager', 'services/DeckAndSlides'], function(module, _, sliderPlugins, CurrentSlideManager, DeckAndSlides) {
 
     var path = sliderPlugins.extractPath(module);
 
-    sliderPlugins.registerPlugin('deck.slides', '*', 'deck-navbar', 1).directive('deckNavbar', [
-        '$rootScope', '$location', '$http', 'CurrentSlideManager',
-        function($rootScope, $location, $http, CurrentSlideManager) {
+    sliderPlugins.registerPlugin('deck', '*', 'deck-navbar', 1).directive('deckNavbar', [
+        '$rootScope', '$location', '$http', 'CurrentSlideManager', 'DeckAndSlides',
+        function($rootScope, $location, $http, CurrentSlideManager, DeckAndSlides) {
 
             return {
                 restrict: 'E',
                 scope: {
-                    slides: '=context'
+                    context: '=context'
                 },
                 templateUrl: path + '/deck-navbar.html',
 
-                link: function($scope) {
-                    $scope.csm = CurrentSlideManager;
-                    $scope.addSlide = function() {
+                link: function(scope) {
+                    scope.csm = CurrentSlideManager;
+                    DeckAndSlides.slides.then(function (slides) {
+                        scope.slides = slides;
+                    });
+                    DeckAndSlides.deck.then(function (deck) {
+                        scope.deck = deck;
+                    });
+
+                    scope.addSlide = function() {
                         // Update deck
-                        $scope.deck.slides.push({
+                        var newSlide = {
                             id: 'empty' + new Date(),
                             name: 'New slide'
+                        };
+
+                        $http.post('/api/slides', newSlide).success( function (data, status) {
+                            scope.slides = scope.slides.concat(newSlide);
+                            scope.deck.slides = scope.deck.slides.concat(data);
+                            $http.put('/api/decks/' + scope.deck._id, scope.deck);
                         });
-                        // TODO [ToDr] TEMP hack
-                        $http.put('/api/decks/' + slides, $scope.deck);
                     };
                 }
             };
