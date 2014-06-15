5006fbbb22b3936378bc0b977079dfd9e9f89685
Slawek Kaczorowski <slawkacz@gmail.com>
2014-06-15 20:30:47 +0200

    added round-robin, logic moved to backend, starting speeddating for all users at the same time, peerjs call

--- platform/public/plugins/slide-speedDating/slide-speedDating.js
+++ platform/public/plugins/slide-speedDating/slide-speedDating.js
@@ -3,8 +3,8 @@ define(['module', '_', 'slider/slider.plugins', 'howler', 'peerjs', 'services/Us
     var path = sliderPlugins.extractPath(module);
 
     sliderPlugins.registerPlugin('slide', 'speedDating', 'slide-speed-dating', 3000).directive('slideSpeedDating', [
-        '$timeout',
-        function ($timeout) {
+        '$timeout', 'Sockets','DeckAndSlides',
+        function ($timeout, Sockets,DeckAndSlides) {
 
             var sounds = new howler.Howl({
                 urls: [path + '/sounds/sprite.mp3', path + '/sounds/sprite.wav'],
@@ -22,9 +22,14 @@ define(['module', '_', 'slider/slider.plugins', 'howler', 'peerjs', 'services/Us
                 templateUrl: path + '/slide-speedDating.html',
                 link: function (scope, element) {
                     _.defaults(scope.speedDating, {
-                        time: 15,
-                        perPerson: 40
+                        time: 5,
+                        perPerson: 10
                     });
+                    // test values
+
+                    scope.speedDating.time = 5;
+                    scope.speedDating.perPerson = 10;
+
                     var toInt = function (x) {
                         var i = parseInt(x, 10);
                         return isNaN(i) ? 0 : i;
@@ -38,73 +43,97 @@ define(['module', '_', 'slider/slider.plugins', 'howler', 'peerjs', 'services/Us
 
                     scope.session = {
                         running: false,
-                        timeLeft: 5
+                        timeLeft: 5,
+                        type: 0,
+                        left: scope.interactions,
+                        round:0
                     };
 
                     scope.testPlay = function (what) {
                         sounds.play(what);
                     };
-                    scope.startDating = function () {
-                        var session = {
-                            running: true,
-                            type: 0,
-                            left: scope.interactions,
-                            endDate: new Date()
-                        };
-
-                        var updateTimeLeft = function () {
-                            session.timeLeft = (session.endDate.getTime() - new Date().getTime()) / 1000;
-                            return session.timeLeft <= 0;
-                        };
-
-                        var updateSession = function () {
-                            session.type = (session.type + 1) % 4;
-                            if (session.type === 0) {
-                                session.left--;
-                            }
-                        };
-                        var playSound = function () {
-                            var toPlay = ['newPerson', 'voiceSwap'];
-                            sounds.play(toPlay[session.type / 2]);
-                        };
 
 
-                        var timer = function (self, time, other, starting) {
-                            if (starting) {
-                                session.endDate = new Date(new Date().getTime() + time * 1000);
-                            }
-
-                            var isFinished = updateTimeLeft();
-                            if (isFinished) {
-
-                                updateSession();
-                                playSound();
-                                // ending
-                                if (session.left) {
-                                    other();
-                                } else {
-                                    session.running = false;
-                                }
-                            } else {
-                                $timeout(self(), 200);
-                            }
-                        };
-
-                        var change = timer.bind(null, function () {
-                            return change;
-                        }, 5, function () {
-                            talking(true);
-                        });
-                        var talking = timer.bind(null, function () {
-                            return talking;
-                        }, toInt(scope.speedDating.perPerson), function () {
-                            change(true);
-                        });
-
-                        scope.session = session;
-                        sounds.play('newPerson');
-                        change(true);
+                    var session = {
+                        running: true,
+                        type: 0,
+                        left: scope.interactions,
+                        round:0,
+                        endDate: new Date()
                     };
+                    scope.startDating = function () {
+                         Sockets.emit('speedDating.startDating');
+                    };
+                    var updateTimeLeft = function () {
+                        scope.session.timeLeft = (scope.session.endDate.getTime() - new Date().getTime()) / 1000;
+                        return scope.session.timeLeft <= 0;
+                    };
+
+                    var updateSession = function () {
+                        scope.session.type = (scope.session.type + 1) % 2;
+                        if (scope.session.type === 0) {
+                            scope.session.left--;
+                            scope.session.round++;
+                        }
+                    };
+                    var playSound = function () {
+                        var toPlay = ['newPerson', 'voiceSwap'];
+                        sounds.play(toPlay[scope.session.type / 2]);
+                    };
+
+
+                    var timer = function (self, time, other, starting) {
+                        if (starting) {
+                            scope.session.endDate = new Date(new Date().getTime() + time * 1000);
+                        }
+
+                        var isFinished = updateTimeLeft();
+                        if (isFinished) {
+
+                            updateSession();
+                            playSound();
+                            // ending
+                            if (scope.session.left) {
+                                other();
+                            } else {
+                                scope.session.running = false;
+                                Sockets.emit('speedDating.finish');
+                            }
+                        } else {
+                            $timeout(self(), 200);
+                        }
+                    };
+
+                    var change = timer.bind(null, function () {
+                        return change;
+                    },  toInt(scope.speedDating.time), function () {
+                        Sockets.emit('speedDating.nextRound',{round: scope.session.round});
+                        // get round start check qt table and start calling.
+                        talking(true);
+                    });
+                    var talking = timer.bind(null, function () {
+                        return talking;
+                    }, toInt(scope.speedDating.perPerson), function () {
+                        // destroy calling
+                        change(true);
+                    });
+                    Sockets.on('speedDating.startDating',function(){
+                        change(true);
+                        sounds.play('newPerson');
+                    });
+
+                    var queueTable = [];
+                    Sockets.on('speedDating.roundRobin', function (qT) {
+                        queueTable = qT;
+                        scope.interactions = queueTable.length;
+                        scope.session.left = scope.interactions;
+                        scope.$digest();
+                    });
+
+                    DeckAndSlides.deck.then(function (data) {
+                        Sockets.emit('speedDating.countQueues',
+                            {deck: data._id, slide: scope.slide.id},function () {});
+                    });
                 }
             };
         }
@@ -116,14 +145,14 @@ define(['module', '_', 'slider/slider.plugins', 'howler', 'peerjs', 'services/Us
                 connections: []
             };
             Sockets.on('speedDating.connections', function (connections) {
-                console.log('here', connections);
                 $scope.sd.connections = connections;
                 $scope.$digest();
             });
+            var MyMediaStream;
             var setMyVideo = function () {
                 navigator.getUserMedia({video: true}, function (MediaStream) {
+                    MyMediaStream = MediaStream;
                     DeckAndSlides.deck.then(function (data) {
-                        console.log(data);
                         Sockets.emit('speedDating.connected', {deck: data._id, slide: $scope.slide.id});
                     });
                     document.querySelector('video.me').src = URL.createObjectURL(MediaStream);
@@ -132,6 +161,7 @@ define(['module', '_', 'slider/slider.plugins', 'howler', 'peerjs', 'services/Us
                     console.log('error', error);
                 });
             };
+            var callConnection;
             var connectToPeer = function (userData) {
                 var username = userData.name.replace(/[^\w|-]|\s/g, '_');
                 peer = new Peer(username, {key: 'k7vpr9xlmbc323xr', config: {'iceServers': [
@@ -147,7 +177,30 @@ define(['module', '_', 'slider/slider.plugins', 'howler', 'peerjs', 'services/Us
                 peer.on('close', function () {
                     // destroy connection
                 });
+                peer.on('call', function(call) {
+                    // Answer the call, providing our mediaStream
+                    callConnection = call;
+                    callConnection.on('stream', function(stream) {
+                        document.querySelector('video.other').src = URL.createObjectURL(stream);
+                    });
+                    callConnection.answer(MyMediaStream);
+                });
             };
+
+            Sockets.on('speedDating.startRound',function(data){
+                if(callConnection && callConnection.close)
+                    callConnection.close();
+                if(data.type === 'call') {
+                    callConnection = peer.call(data.user.username.replace(/[^\w|-]|\s/g, '_'), MyMediaStream);
+                    callConnection.on('stream', function(stream) {
+                        document.querySelector('video.other').src = URL.createObjectURL(stream);
+                    });
+                }
+            });
+            Sockets.on('speedDating.finish',function(){
+                if(callConnection && callConnection.close)
+                    callConnection.close();
+            });
             User.setCallback(connectToPeer);
             User.getUserData();
         }]);
