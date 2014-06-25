1ac1baab86dd5ce44199cd8c3d95604aed9e234d
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-06-25 07:06:45 +0200

    common changes

---
+++ platform/app/models/snapshot.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+Schema = mongoose.Schema;
+
+var Snapshot = new Schema({
+    //slide
+});
---
+++ platform/app/plugins/slide-liveSave/slide-liveSave.js
@@ -0,0 +1,20 @@
+var lz-string = require('lz-string');
+
+
+
+exports.onSocket = function (log, socket, io) {
+    
+    var saveCode = function (data, res) {
+        
+    };
+
+    var loadCode = function (data, res) {
+    
+    };
+
+    socket.on('slide.code.save', saveCode);
+    socket.on('slide.code.load', loadCode);
+};
+
+
+
