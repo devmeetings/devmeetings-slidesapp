c3e6307b39459e963c7648a0fd394c5088f5f1a1
Tomusdrw <tomusdrw@gmail.com>
2014-06-26 07:53:07 +0200

    Bitcoin plugin - ondemand.

--- platform/public/plugins/deck.sidebar-bitcoin/deck.sidebar-bitcoin.js
+++ platform/public/plugins/deck.sidebar-bitcoin/deck.sidebar-bitcoin.js
@@ -1,7 +1,7 @@
 define(['module', 'slider/slider.plugins'], function(module, sliderPlugins) {
     var path = sliderPlugins.extractPath(module);
 
-    sliderPlugins.registerPlugin('deck.sidebar', '*', 'decksidebar-bitcoin').directive('decksidebarBitcoin', [
+    sliderPlugins.registerPlugin('deck.sidebar', 'bitcoin', 'decksidebar-bitcoin').directive('decksidebarBitcoin', [
 
         function() {
             return {
