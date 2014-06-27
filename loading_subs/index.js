613c00d26aa67bd341171e73ad33eb2abf2815ab
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-06-27 11:42:55 +0200

    comon changes

--- loading_subs/index.js
+++ loading_subs/index.js
@@ -1,4 +1,16 @@
-var parser = require('subtitles-parser');
-var fs = require('fs');
+var parser = require('subtitles-parser'),
+    fs = require('fs'),
+    MongoClient = require('mongodb').MongoClient;
 
+var config = require('../platform/config/config');
 
+MongoClient.connect(config.db, function (err, db) {
+    if (err) {
+        throw err;
+    }
+
+    db.collection('snapshots').find().toArray( function (err, results) {
+        console.dir(results);
+        db.close();
+    });
+});
