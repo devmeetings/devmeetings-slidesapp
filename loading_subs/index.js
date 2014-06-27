bc3a666d56e78920da1f31e272741bdfaa1d449a
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-06-27 16:41:35 +0200

    parsing snapshots

--- loading_subs/index.js
+++ loading_subs/index.js
@@ -5,23 +5,60 @@ var fs = require('fs'),
 
 var config = require('../platform/config/config');
 
+var keysToTrim = ['live-save', 'toolbar', 'commit'];
+
+var snapshotTrimmer = function (object) {
+    var keys = _.keys(object);
+    for (var i = 0; i < keys.length; i++){
+        var key = keys[i];
+        if (_.contains(keysToTrim, key)){
+            delete object[key];
+        } else {
+            snapshotTrimmer(object[key]);
+        }
+    }
+};
+
 MongoClient.connect(config.db, function (err, db) {
     if (err) {
         throw err;
     }
 
     db.collection('snapshots').find().toArray( function (err, results) {
-        var datas = _.pluck(results, 'data');
 
-        var snaps = [];
-        _.forEach(datas, function (elem) {
-            var decompressed = JSON.parse(LZString.decompressFromBase64(elem));
-            snaps = snaps.concat(decompressed);
-        }, []);
+        var sortedResults = _.sortBy(results, function (elem) {
+            return elem.timestamp;
 
-        console.log(snaps);
-        console.log(_.pluck(_.pluck(snaps, 'code'), 'code'));
+        });
+        var snaps = _.reduce(sortedResults, function (acc, elem) {
+            var last = _.last(acc);
+            if (last !== undefined && last.slideId === elem.slideId.toString() && last.userId === elem.userId && elem.timestamp < last.timestamp + 60000){
+                last.timestamp = elem.timestamp;
+                last.slides = last.slides.concat(JSON.parse(LZString.decompressFromBase64(elem.data)));
+            } else {
+                last = {
+                    slideId: elem.slideId.toString(),
+                    userId: elem.userId,
+                    timestamp: elem.timestamp,
+                    slides: JSON.parse(LZString.decompressFromBase64(elem.data))
+                };
+                acc.push(last);
+            }
+            return acc;
+        }, [
+        ]);
 
+        snapshotTrimmer(snaps);
+        var finalSnaps = _.map(snaps, function (snap) {
+            var beginTime = snap.slides[0].timestamp;
+            _.forEach(snap.slides, function (slide) {
+                slide.timestamp -= beginTime;
+            });
+            return snap.slides;
+        });
+
+        //fs.writeFile('testOutput', JSON.stringify(finalSnaps));
+        //console.log(finalSnaps);
         db.close();
     });
 });
