33d098fffe80fb77daf36f7b99f491b88699472f
Tomusdrw <tomusdrw@gmail.com>
2014-07-02 11:10:02 +0200

    Video synchronization. Attempt #1.

--- platform/public/xplatform/player/xplatform-player.js
+++ platform/public/xplatform/player/xplatform-player.js
@@ -2,24 +2,66 @@ define(['angular', '_', 'video-js', 'video-js-youtube', 'xplatform/xplatform-app
     function(angular, _, videojs, videojsyoutube, xplatformApp, Recordings, RecordingsPlayerFactory, sliderPlugins) {
 
         angular.module('xplatform').directive('videojs', [
-
-            function() {
+            '$timeout',
+            function($timeout) {
 
                 return {
                     restrict: 'E',
                     scope: {
                         src: '=',
-                        currentSecond: '='
+                        currentSecond: '=',
+                        isPlaying: '=',
+                        shouldUpdateTime: '@',
+                        controls: '@'
                     },
-                    template: '<div><video class="video-js vjs-default-skin" controls preload="auto" width="512" height="211"></video></div>',
+                    template: '<div><video class="video-js vjs-default-skin" preload="auto" width="512" height="211"></video></div>',
                     link: function(scope, element) {
-                        var player = videojs(element.find('video')[0], {
+
+                        var $video = element.find('video')[0];
+                        if (scope.controls) {
+                            $video.setAttribute('controls', true);
+                        }
+                        var player = videojs($video, {
                             techOrder: ['youtube'],
                             src: scope.src
                         });
-                        player.on('timeupdate', function() {
+                        if (scope.shouldUpdateTime) {
+                            player.on('timeupdate', function() {
+                                scope.$apply(function() {
+                                    scope.currentSecond = player.currentTime();
+                                });
+                            });
+                        }
+
+                        player.on('play', function() {
                             scope.$apply(function() {
-                                scope.currentSecond = player.currentTime();
+                                scope.isPlaying = true;
+                            });
+                        });
+
+                        player.on('pause', function() {
+                            scope.$apply(function() {
+                                scope.isPlaying = false;
+                            });
+                        });
+
+                        scope.$watch('isPlaying', function(newVal, oldVal) {
+                            if (newVal === oldVal) {
+                                return;
+                            }
+
+                            $timeout(function() {
+                                if (scope.isPlaying) {
+                                    if (!player.paused()) {
+                                        player.play();
+                                    }
+                                    player.currentTime(Math.round(scope.currentSecond));
+                                } else {
+                                    if (player.paused()) {
+                                        return;
+                                    }
+                                    player.pause();
+                                }
                             });
                         });
                     }
