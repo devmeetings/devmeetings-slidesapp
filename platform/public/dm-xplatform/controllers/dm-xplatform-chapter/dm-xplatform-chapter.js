2777e91e451b45e60f75b851e4002e59e2197ac1
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-07-23 14:30:36 +0200

    player video

--- platform/public/dm-xplatform/controllers/dm-xplatform-chapter/dm-xplatform-chapter.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-chapter/dm-xplatform-chapter.js
@@ -1,9 +1,18 @@
 define(['angular',
         'xplatform/xplatform-app'
 ], function (angular, xplatformApp) {
-    xplatformApp.controller('dmXplatformChapter', ['$scope', '$stateParams', 'dmTrainings',
-        function ($scope, $stateParams, dmTrainings) {
-            $scope.chapterIndex = $stateParams.index;
+    xplatformApp.controller('dmXplatformChapter', ['$scope', '$stateParams', '$http', 'dmTrainings',
+        function ($scope, $stateParams, $http, dmTrainings) {
+            var trainingId = $stateParams.id;
+            var chapterIndex = $stateParams.index;
+
+            dmTrainings.getTrainingWithId(trainingId).then (function (training) {
+                $scope.chapter = training.chapters[chapterIndex];
+                $http.get('/api/recordings/' + $scope.chapter.videodata.recording)
+                .success(function (recording) {
+                    $scope.recording = recording;    
+                });
+            });
         }
     ]);
 });
