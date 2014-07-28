cad03bb907b31ad31c35812e4b758d6e5adfdab4
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-07-28 12:45:05 +0200

    live table

--- platform/public/dm-xplatform/controllers/dm-xplatform-live/dm-xplatform-live.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-live/dm-xplatform-live.js
@@ -1,7 +1,9 @@
 define(['angular', 'xplatform/xplatform-app'], function (angular, xplatformApp) {
     xplatformApp.controller('dmXplatformLive', ['$scope', '$http',
         function ($scope, $http) {
-            
+            $http.get('/api/events').success(function (events) {
+                $scope.events = events;
+            });
         }
     ]);
 });
