e8a78b07c067889995527a4c7b6b46553ffab268
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-07-30 14:24:48 +0200

    stream list

--- platform/public/dm-xplatform/controllers/dm-xplatform-stream/dm-xplatform-stream.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-stream/dm-xplatform-stream.js
@@ -9,5 +9,33 @@ define(['angular', 'xplatform/xplatform-app'], function (angular, xplatformApp) 
                 $scope.stream = data;
             });
         }
+
+        var details = {
+            'event.signup' : {
+                text: 'weźmie udział w szkoleniu'
+            },
+            'deck.start' : {
+                text: 'rozpoczął kurs'
+            },
+            'deck.finish' : {
+                text: 'zakończył kurs'
+            },
+            'slide.enter' : {
+                text: 'rozpoczął rozwiązywanie rozwiazywanie zadań na slajdzie'
+            },
+            'video.enter' : {
+                text: 'video ogląda szkolenie video'
+            },
+            'microtask.done' : {
+                text: 'ukończył microzadanie'
+            }
+        };
+
+        $scope.detailsForActivity = function (activity) {
+            var det = details[activity.type];
+            det.title = activity.data.title; 
+            return det;
+        };
+
     }]);
 });
