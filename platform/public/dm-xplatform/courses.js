7871d05537d572d77c3b5c2e66b5abece5bfacee
Tomusdrw <tomusdrw@gmail.com>
2014-08-23 20:05:47 +0200

    Descriptions 1.

--- platform/public/dm-xplatform/courses.js
+++ platform/public/dm-xplatform/courses.js
@@ -4,6 +4,11 @@ define(['xplatform/xplatform-app'], function(xplatformApp) {
         var img = function(img) {
             return "static/images/workshopdesc/" + img + ".jpg";
         };
+        var multiline = function(func) {
+            var f = func.toString();
+            var start = f.indexOf('/*-') + 3;
+            return f.substr(start, f.indexOf('-*/') - start);
+        };
         return {
             getCourseById: function(id) {
                 return this.courses.filter(function(x) {
@@ -14,43 +19,118 @@ define(['xplatform/xplatform-app'], function(xplatformApp) {
                 id: 'ampersand',
                 tags: ['ampersand', 'backbone', 'react', 'web', 'mvc', 'js'],
                 title: 'Ampersand.js - następca Backbone’a?',
-                description: 'Opis frameworku Ampersand.js.\n' +
-                    'Czym różni się od Backbone?\n' +
-                    'Czy warto przepisać swoją aplikację na Ampersand.js?\n' +
-                    'Porównanie Ampersand.js do AngularJS i React.\n',
+                description: multiline(function() {
+                    /*-
+* Co wnosi Ampersand.js?
+* Czym różni się od Backbone?
+* W jaki sposób budować aplikacje oparte o Ampersand.js.
+* Porównanie Ampersand.js do AngularJS i React
+                -*/
+                }),
                 image: img('ampersandjs'),
-                progressbarValue: 100
+                progressbarValue: 3
             }, {
                 id: 'angular-directives',
+                tags: ['angular', 'directives', 'web', 'js', 'mvc'],
                 order: -1,
                 status: 'captured',
-                image: img('angular_directives')
+                title: 'Dyrektywy w AngularJS',
+                description: multiline(function() {
+                    /*-
+Dyrektywy w AngularJS są klejem pomiędzy modelem a drzewem DOM.
+
+* Dowiedz się jak zbudowane są wewnątrzne dyrektywy takie jak `ngClick` i `ngModel`.
+* Przekonaj się dlaczego warto izolować `scope` dyrektyw.
+* Naucz się pisać dyrektywy dla własnych komponentów.
+* Zobacz jak zintegrować plugin jQuery w postaci dyrektywy.
+                -*/
+                }),
+                image: img('angular_directives'),
+                progressbarValue: 91
             }, {
                 id: 'ember',
                 tags: ['ember', 'angular', 'backbone', 'js', 'web', 'mvc'],
                 title: 'Ember.js vs AngularJS vs Backbone.js',
-                description: 'Którego frameworku użyć w kolejnym projekcie? A może React?\n' +
-                    'Porównanie trzech najpopularniejszych frameworków MV*.\n' +
-                    'Dla każdego frameworku przeanalizujemy przykłady aplikacji, w których sprawdza się najlepiej\n',
+                description: multiline(function() {
+                    /*-
+Obecnie w świecie JSowym dostępnych jest sporo rozwiązań realizujących wzorzec MV*.
+
+Dowiedz się:
+
+* którego frameworku użyć w kolejnym projekcie,
+* czy warto korzystać z frameworku MV*,
+* jak do wymienionych frameworków ma się React,
+* czym jest Two-Way Binding,
+* do jakich zastosowań nadaje się każdy z nich,
+
+Dla każdego frameworku przeanalizujemy przykłady aplikacji, w których sprawdza się najlepiej.
+                -*/
+                }),
                 image: img('angular_ember_backbone'),
-                progressbarValue: 44
+                progressbarValue: 14
             }, {
                 id: 'angular-forms',
                 order: -1,
+                tags: ['js', 'angular', 'web', 'forms', 'html5'],
                 status: 'captured',
+                title: 'Formularze w AngularJS',
+                description: multiline(function() {
+                    /*-
+Na kursie wykorzystamy walidację formularzy, którą oferuje AngularJS do stworzenia ekranu rejestracji.
+
+Dowiemy się jak można określić czy formularz jest wypełniony poprawnie oraz zastosujemy Bootstrapa
+do wyświetlenia błędów walidacji.
+
+* Walidacja `minLength` i `maxLength`
+* Walidacja za pomocą regexpa
+* Walidacje pól typu `email` i `number`
+                    -*/
+                }),
+                progressbarValue: 91,
                 image: img('angular_forms')
             }, {
                 id: 'angular-intro',
                 order: -2,
                 tags: ['js', 'angular', 'web'],
-                title: 'AngularJS - podstawy',
+                title: 'Wprowadzenie do AngularJS',
                 status: 'ready',
-                description: '',
+                description: multiline(function() {
+                    /*-
+Podstawy AngularJS
+
+* Expressions
+* Kontrolery
+* `ngRepeat`
+* Filtry: `filter`, `orderBy` i `limitTo`
+* Kontrola widoczności i wyglądu: `ngClass` i `ngShow`
+                    -*/
+                }),
+                progressbarValue: 100,
                 image: img('angular_intro')
             }, {
                 id: 'angular-route',
                 order: -1,
+                tags: ['js', 'angular', 'route'],
                 status: 'captured',
+                title: 'Routing w AngularJS',
+                description: multiline(function() {
+                    /*-
+
+                    -*/
+                })
+                progressbarValue: 91,
+                image: img('angular_route')
+            }, {
+                id 'angular-uiRouter',
+                order: -1,
+                title: 'Zaawansowany routing w AngularJS z użyciem uiRouter',
+                tags: ['angular', 'js', 'route', 'ui'],
+                description: multiline(function() {
+                    /*-
+
+                    -*/
+                }),
+                progressbarValue: 3,
                 image: img('angular_route')
             }, {
                 id: 'angular-services',
