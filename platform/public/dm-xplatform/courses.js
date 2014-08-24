032b2628cde7de02b4b5c0d384e29f916ca43115
Tomusdrw <tomusdrw@gmail.com>
2014-08-24 13:21:39 +0200

    Courses descriptions.

--- platform/public/dm-xplatform/courses.js
+++ platform/public/dm-xplatform/courses.js
@@ -115,18 +115,29 @@ Podstawy AngularJS
                 title: 'Routing w AngularJS',
                 description: multiline(function() {
                     /*-
+Dowiedz się jak zarządzać ekranami w Single Page Application opartej o AngularJS.
+
+* Jak wykorzystać URLe do zmieniania widoku
+* Jak mapować routing do kontrolerów i template'ów.
+* Jak parametryzować URLe i odbierać parametry w kontrolerach.
 
                     -*/
-                })
+                }),
                 progressbarValue: 91,
                 image: img('angular_route')
             }, {
-                id 'angular-uiRouter',
+                id: 'angular-uiRouter',
                 order: -1,
                 title: 'Zaawansowany routing w AngularJS z użyciem uiRouter',
                 tags: ['angular', 'js', 'route', 'ui'],
                 description: multiline(function() {
                     /*-
+Poznaj jak zarządzać stanami Twojej aplikacji z użyciem `uiRouter`.
+
+* Jaką przewagę `uiRouter` ma nad `ngRoute`?
+* Jak definiować stany i URLe?
+* Jak tworzyć linki do określonego stanu.
+* Jak parametryzować stany.
 
                     -*/
                 }),
@@ -136,140 +147,304 @@ Podstawy AngularJS
                 id: 'angular-services',
                 order: -1,
                 status: 'captured',
+                tags: ['angular', 'js', 'web', 'service'],
+                title: 'Serwisy w AngularJS',
+                description: multiline(function() {
+                    /*-
+* W jaki sposób można tworzyć serwisy w Angularze i do czego je wykorzystywać?
+* Jak radzić sobie z dużymi aplikacjami w AngularJS?
+* Jak organizować  i porządkować kod w aplikacji.
+* Czym są `promise`?
+
+                    -*/
+                }),
+                progressbarValue: 91,
                 image: img('angular_services')
             }, {
                 id: 'angular-nobackend',
                 tags: ['angular', 'backend', 'js', 'production', 'api', 'web', 'spa'],
                 title: 'Pisanie bezbackendowych apek w AngularJS',
-                description: 'Czy potrzebujemy backendu? W jaki sposób możemy pisać aplikacje webowe z wykorzystaniem API udostępnianych przez zewnętrzne serwisy.\n' +
-                    '- Obsługa baz danych\n' +
-                    '- Autentykacja / Autoryzacja\n' +
-                    '- Gdzie hostować statyczny frontend?\n' +
-                    '- Obsługa aplikacji offline - Manifest, ServiceWorker\n',
+                description: multiline(function() {
+                    /*-
+
+Czy potrzebujemy backendu? W jaki sposób możemy pisać aplikacje webowe z wykorzystaniem API udostępnianych przez zewnętrzne serwisy.
+
+* Obsługa persystencji (baz danych)
+* Autentykacja / Autoryzacja
+* Gdzie hostować statyczny frontend?
+* Obsługa aplikacji offline - `manifest`, `ServiceWorker`
+
+                    -*/
+                }),
                 image: img('angular_intro'),
-                progressbarValue: 60
+                progressbarValue: 23
             }, {
                 id: 'backbone',
                 tags: ['backbone', 'web', 'js', 'production', 'spa', 'mvc'],
                 title: 'Backbone.js - produkcyjne pisanie SPA',
-                description: 'Jak wygląda “produkcyjna” praca w Backbone.js? \n' +
-                    'Jak organizować kod, z jakich korzystać pluginów? \n' +
-                    'Jak radzić sobie z rosnącymi zależnościami i błędami programistów?\n' +
-                    'Jakich narzędzi używać do szukania błędów i poprawiania wydajności.\n' +
-                    'Przedstawienie zestawu dobrych praktyk do zaaplikowania w swoich rozwiązaniach.\n',
+                description: multiline(function() {
+                    /*-
+* Jak wygląda “produkcyjna” praca w Backbone.js?
+* Jak organizować kod, z jakich korzystać pluginów?
+* Jak radzić sobie z rosnącymi zależnościami i błędami programistów?
+* Jakich narzędzi używać do szukania błędów i poprawiania wydajności.
+* Przedstawienie zestawu dobrych praktyk do zaaplikowania w swoich rozwiązaniach.
+
+                    -*/
+                }),
                 image: img('backbone'),
                 progressbarValue: 90
             }, {
                 id: 'bootstrap',
+                tags: ['bootstrap', 'rwd', 'web', 'html'],
+                title: 'Budowanie interfejsów z użyciem Twitter Bootstrap',
+                description: multiline(function() {
+                    /*-
+* Dlaczego warto nauczyć się Bootstrapa?
+* Omówienie dostępnych komponentów i filozofi Bootstrapa.
+* RWD z użyciem Bootstrapa.
+* Tworzenie `themes` - własny wygląd.
+* Jak nazywać własne klasy aby nie konfliktować z Nootstrapem?
+
+                    -*/
+                }),
                 image: img('bootstrap'),
+                progressbarValue: 9
             }, {
                 id: 'cassandra',
                 tags: ['nosql', 'db', 'cassandra'],
                 title: 'Modelowanie danych dla Apache Cassandra',
-                description: 'Jak wygląda modelowanie danych dla Cassandry? Czym różni się Cassandra od MongoDB.\n' +
-                    'Do jakich zastosowań możemy stosować Cassandrę?\n',
+                description: multiline(function() {
+                    /*-
+* Jak wygląda modelowanie danych dla Cassandry? 
+* Czym różni się Cassandra od MongoDB.
+* Do jakich zastosowań możemy stosować Cassandrę?
+* Porównanie wydajności Cassandy, MongoDB i SQL w różnych zastosowaniach.
+
+                    -*/
+                }),
                 image: img('cassandra'),
-                progressbarValue: 93
+                progressbarValue: 0
             }, {
                 id: 'erlang-oo',
                 tags: ['erlang', 'functional', 'oo'],
                 title: 'Podstawy Erlanga dla obiektowców',
-                description: '- Wstęp z języka Erlang dla programistów języków obiektowych.\n' +
-                    '- Opis podejścia funkcyjnego - brak efektów ubocznych\n' +
-                    '- Procesy\n' +
-                    '- Hot Reload\n' +
-                    '- Unit testing\n' +
-                    '- Production deployment\n',
+                description: multiline(function() {
+                    /*-
+Wstęp z języka Erlang dla programistów języków obiektowych.
+
+* Opis podejścia funkcyjnego - brak efektów ubocznych
+* Procesy i komunikacja między nimi
+* Hot Reload
+* Unit testing
+* Production deployment
+
+                    -*/
+                }),
                 image: img('erlang'),
-                progressbarValue: 10
+                progressbarValue: 16
             }, {
                 id: 'erlang-node',
                 title: 'Erlang dla node’owców',
                 tags: ['erlang', 'functional', 'node', 'js'],
-                description: '- W jaki sposób przenieść aplikację nodejs do Erlanga.\n' +
-                    '- Interkomunikacja Erlang <> Nodejs\n' +
-                    '- Co może dać Erlang?\n',
+                description: multiline(function() {
+                    /*-
+Co wspólnego mają ze sobą Erlang i Nodejs?
+
+* W jaki sposób przenieść aplikację nodejs do Erlanga.
+* Interkomunikacja Erlang <> Nodejs
+* Co może dać Erlang?
+                    -*/
+                }),
                 image: img('erlang_node'),
-                progressbarValue: 20
+                progressbarValue: 6
             }, {
                 id: 'express',
+                tags: ['express', 'node', 'js', 'web'],
+                title: 'Aplikacje webowe w Express',
+                description: multiline(function() {
+                    /*-
+Kurs budowania aplikacji webowych w oparciu o framework Express.
+
+* Jak zorganizować kod aplikacji?
+* Co daje nam full stack JavaScript?
+* Jak współdzielić kod między frontendem i backendem?
+* Jak tworzyć RESTowe API?
+
+                    -*/
+                }),
                 image: img('express'),
+                progressbarValue: 55
             }, {
                 id: 'haskell',
                 tags: ['haskell', 'functional', 'js'],
                 title: 'Haskell dla JavaScriptowców Funkcyjnych',
-                description: '- Dogłębne zrozumienie koncepcji dostępnych w JS na przykładzie języka Haskell\n' +
-                    '- Co wspólnego mają ze sobą Haskell i JavaScript\n' +
-                    '- Porównanie rozwiązań problemów w obu językach\n',
+                description: multiline(function() {
+                    /*-
+
+* Dogłębne zrozumienie koncepcji dostępnych w JS na przykładzie języka Haskell.
+* Co wspólnego mają ze sobą Haskell i JavaScript?
+* Porównanie rozwiązań problemów w obu językach.
+
+                    -*/
+                }),
                 image: img('haskell_js'),
                 progressbarValue: 40
             }, {
                 id: 'java-8',
+                tags: ['java', 'lambda', 'functional'],
+                title: 'Programowanie funkcyjne w Java 8',
+                description: multiline(function() {
+                    /*-
+Myślisz o przesiadce na Javę 8? Zobacz co ciekawego niesie ze sobą ten rewolucyjny update!
+
+* Lambdy w Java 8.
+* Jak korzystać ze Stream API?
+* Nowe API do czasu i dat.
+* Nowe możliwości a wydajność.
+
+                    -*/
+                }),
+                progressbarValue: 11,
                 image: img('java_18_lambda')
             }, {
                 id: 'javascript-oop',
+                tags: ['js', 'oop', 'web'],
+                title: 'Programowanie obiektowe w JS',
+                description: multiline(function() {
+                    /*-
+Programowanie obiektowe dla zaawansowanych JavaSkryptowców.
+
+* `module` pattern
+* Organizacja kodu w namespace'y
+* Implementacja klas i dziedziczenia.
+
+                    -*/
+                }),
+                progressbarValue: 70,
                 image: img('javascript_oop')
             }, {
                 id: 'jsfrep',
                 tags: ['js', 'functional', 'events', 'ramda', 'web'],
                 title: 'Functional Reactive Programming w JS',
-                description: '- Wstęp do programowania funkcyjnego\n' +
-                    '- Wstęp do programowania reaktywnego\n' +
-                    '- Modelowanie problemów w postaci strumieni eventów\n' +
-                    '- Biblioteka RamdaJS\n',
+                description: multiline(function() {
+                    /*-
+* Wstęp do programowania funkcyjnego
+* Wstęp do programowania reaktywnego
+* Modelowanie problemów w postaci strumieni eventów
+* Biblioteka RamdaJS
+
+                    -*/
+                }),
                 image: img("jsprog"),
                 progressbarValue: 30
             }, {
                 id: 'koa',
                 tags: ['koa', 'js', 'web', 'node', 'ecma'],
                 title: 'Koa + EcmaScript 6',
-                description: 'Framework koa i opis EcmaScript 6',
+                description: multiline(function() {
+                    /*-
+Koa - następca Express. Co zmieni się w JS po wprowadzeniu Harmony (ES6)?
+
+* Wpływ generatorów na programowanie asynchroniczne.
+* Klasy - czy są potrzebne?
+* Nowe struktury danych: `Set` i `Map`
+* Rekurencja ogonowa
+
+                    -*/
+                }),
                 image: img('koa'),
                 progressbarValue: 0
             }, {
                 id: 'mongodb',
                 title: 'MongoDB + Elasticsearch',
                 tags: ['mongodb', 'db', 'elasticsearch', 'nosql'],
-                description: '- Czym są bazy NoSQL\n' +
-                    '- Czym różni się modelowanie danych dla baz SQL i NoSQL\n' +
-                    '- W jaki sposób łączyć ze sobą dostępne produkty, aby w najlepszy sposób rozwiązywać problemy pojawiające się w dużych aplikacjach.\n',
+                description: multiline(function() {
+                    /*-
+* Czym są bazy NoSQL
+* Czym różni się modelowanie danych dla baz SQL i NoSQL
+* W jaki sposób łączyć ze sobą dostępne produkty, aby w najlepszy sposób rozwiązywać problemy pojawiające się w dużych aplikacjach.
+
+                    -*/
+                }),
                 image: img('mongodb_elastic'),
-                progressbarValue: 78
+                progressbarValue: 9
             }, {
                 id: 'neo4j',
                 tags: ['db', 'nosql', 'neo4j'],
                 title: 'Neo4J - wprowadzenie do grafowych baz danych',
-                description: '\n',
+                description: multiline(function() {
+                    /*-
+Modelowanie relacji w bazach grafowych na przykładzie Neo4J.
+
+* Jakiego typu dane trzymać w bazach grafowych?
+* Jak łączyć rózne technologie bazowe (Neo4J + Mongo + ElasticSearch)?
+* Jak optymalizować zapytania?
+
+                    -*/
+                }),
                 image: img('neo4j'),
-                progressbarValue: 28
+                progressbarValue: 13
             }, {
                 id: 'ocaml',
                 tags: ['ocaml', 'functional', 'js'],
                 title: 'Programowanie funkcyjne w Ocaml',
-                description: '- Czym jest Ocaml?\n' +
-                    '- Co wspólnego mają ze sobą Ocaml i JavaScript\n' +
-                    '- Czym jest programowanie funkcyjne?\n' +
-                    '- Do czego nadaje się Ocaml?\n',
+                description: multiline(function() {
+                    /*-
+* Czym jest Ocaml?
+* Co wspólnego mają ze sobą Ocaml i JavaScript
+* Na czym polega programowanie funkcyjne?
+* Do czego nadaje się Ocaml?
+                    -*/
+                }),
                 image: img('ocaml'),
-                progressbarValue: 50
+                progressbarValue: 0
             }, {
                 id: 'react-flux',
                 title: 'React + FLUX',
                 tags: ['react', 'mvc', 'flux', 'js', 'web'],
-                description: '- Jaki problem rozwiązuje React?\n' +
-                    '- Jak React ma się do AngularJS a jak do Backbone?\n' +
-                    '- Na czym polega architektura FLUX\n',
+                description: multiline(function() {
+                    /*-
+
+* Jaki problem rozwiązuje React?
+* Jak React ma się do AngularJS a jak do Backbone?
+* Na czym polega architektura FLUX
+                    -*/
+                }),
                 image: img('reactjs_flux'),
-                progressbarValue: 80
+                progressbarValue: 0
             }, {
                 id: 'rwd-ecommerce',
                 title: 'Responsywne e-sklepy',
                 tags: ['rwd', 'css', 'ecommerce', 'web', 'less', 'sass', 'js'],
-                description: 'RWD dla sklepów internetowych. Szczegółowa analiza rozwiązań na podstawie rzeczywistych przykładów i ich wpływ na konwersję.\n',
+                description: multiline(function() {
+                    /*-
+RWD dla sklepów internetowych. 
+
+Szczegółowa analiza rozwiązań na podstawie rzeczywistych przykładów i ich wpływ na konwersję.
+
+* Ile breakpointów powinien mieć Twój design?
+* Jakie elementy są istotne na tablecie, telefonie, a jakie na Desktopie?
+
+                    -*/
+                }),
                 image: img('rwd_commerce'),
-                progressbarValue: 70
+                progressbarValue: 71
             }, {
                 id: 'sass-less',
+                tags: ['sass', 'less', 'css', 'web', 'rwd'],
+                title: 'Preprocesory CSS',
+                description: multiline(function() {
+                    /*-
+Po co korzystać z preprocesorów CSS? Który preprocesor wybrać?
+
+* Zalety preprocesorów - czego brakuje w CSS?
+* Porównanie LESS i SASS.
+* Mixins i zmienne.
+
+                    -*/
+                }),
+                progressbarValue: 34,
                 image: img('sass_less')
             }]
 
