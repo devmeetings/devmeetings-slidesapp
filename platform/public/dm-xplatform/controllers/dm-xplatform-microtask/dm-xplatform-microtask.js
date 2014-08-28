e4c7a225804c443ff6965214aa4b3596048688cd
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-08-28 16:59:01 +0200

    microtasks directive!

--- platform/public/dm-xplatform/controllers/dm-xplatform-microtask/dm-xplatform-microtask.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-microtask/dm-xplatform-microtask.js
@@ -1,4 +1,4 @@
-define(['angular', 'xplatform/xplatform-app', 'xplatform/directives/microtask'], function(angular, xplatformApp) {
+define(['angular', 'xplatform/xplatform-app', 'xplatform/directives/dm-microtask/dm-microtask'], function(angular, xplatformApp) {
     xplatformApp.controller('dmXplatformMicrotask', ['$scope',
         function($scope) {
 
