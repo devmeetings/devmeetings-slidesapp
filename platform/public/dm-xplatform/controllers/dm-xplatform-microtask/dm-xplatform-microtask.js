6ecea4f12222650665a0292127a7f507d9dacedc
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-08-28 17:03:40 +0200

    microinfos

--- platform/public/dm-xplatform/controllers/dm-xplatform-microtask/dm-xplatform-microtask.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-microtask/dm-xplatform-microtask.js
@@ -1,4 +1,8 @@
-define(['angular', 'xplatform/xplatform-app', 'xplatform/directives/dm-microtask/dm-microtask'], function(angular, xplatformApp) {
+define(['angular',
+        'xplatform/xplatform-app',
+        'xplatform/directives/dm-microinfo/dm-microinfo',
+        'xplatform/directives/dm-microtask/dm-microtask'],
+        function(angular, xplatformApp) {
     xplatformApp.controller('dmXplatformMicrotask', ['$scope',
         function($scope) {
 
