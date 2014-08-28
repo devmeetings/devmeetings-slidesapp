2d9a7ec81e1dcefbf191c044542aed941b4bbf89
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-08-28 12:41:24 +0200

    modals fixes

--- platform/public/dm-xplatform/dm-xplatform.js
+++ platform/public/dm-xplatform/dm-xplatform.js
@@ -43,7 +43,8 @@ require(['angular',
                         $modal.open({
                             templateUrl: '/static/dm-xplatform/controllers/dm-xplatform-register/dm-xplatform-register.html',
                             controller: 'dmXplatformRegister',
-                            size: 'sm'
+                            size: 'sm',
+                            windowClass: 'login-modal'
                         });
                         return;
                     }
@@ -51,7 +52,8 @@ require(['angular',
                     $modal.open({
                         templateUrl: '/static/dm-xplatform/controllers/dm-xplatform-login/dm-xplatform-login.html',
                         controller: 'dmXplatformLogin',
-                        size: 'sm'
+                        size: 'sm',
+                        windowClass: 'login-modal'
                     });
                 });
             }]);
