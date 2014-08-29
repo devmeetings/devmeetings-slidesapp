fec3fd7fd8deff60de142bc2c844d178d68e2118
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-08-29 11:55:58 +0200

    microtask subplugin

--- platform/public/dm-xplatform/controllers/dm-xplatform-slide/dm-xplatform-slide.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-slide/dm-xplatform-slide.js
@@ -1,5 +1,7 @@
-define(['angular', 'xplatform/xplatform-app',
-        'directives/plugins-loader'
+define(['angular', 
+        'xplatform/xplatform-app',
+        'directives/plugins-loader',
+        'xplatform/directives/dm-microtask-users/dm-microtask-users'
         ], function (angular, xplatformApp, pluginsLoader) {
     xplatformApp.controller('dmXplatformSlide', ['$scope', '$stateParams', '$state', '$http', 'dmUser', function ($scope, $stateParams, $state, $http, dmUser) {
         $http.get('/api/slides/' + $stateParams.id).success(function (slide) {
---
+++ platform/public/dm-xplatform/directives/dm-microtask-users/dm-microtask-users.js
@@ -0,0 +1,16 @@
+define(['angular', 'xplatform/xplatform-app', 'slider/slider.plugins'], function (angular, xplatformApp, sliderPlugins) {
+    sliderPlugins.registerPlugin('microtask', '*', 'microtask-users', 500).directive('microtaskUsers', [
+        '$http', function ($http) {
+            return {
+                restrict: 'E',
+                scope: {
+                    microtask: '=context'
+                },
+                templateUrl: '/static/dm-xplatform/directives/dm-microtask-users/dm-microtask-users.html',
+                link: function (scope, element) {
+                
+                }
+            }
+        }
+    ]);
+});
