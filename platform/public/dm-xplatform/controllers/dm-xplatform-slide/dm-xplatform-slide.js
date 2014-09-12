d1a2dcfdda3fb382f06a708d365066203b7f76a8
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-09-12 15:21:07 +0200

    name regexp

--- platform/public/dm-xplatform/controllers/dm-xplatform-slide/dm-xplatform-slide.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-slide/dm-xplatform-slide.js
@@ -22,7 +22,7 @@ define(['angular',
                         return;
                     }
 
-                    var name = encodeURI(scope.name.replace(/ /g, ''));
+                    var name = encodeURI(scope.name.replace(/[^0-9a-zA-Z]/g, '_'));
                     var host = 'Burstfire.UK.EU.GameSurge.net';
                     //element[0].src = 'http://localhost:8001/#/?autologin=true&host=irc.freenode.org&port=6667&nick=' + name + '&realname=' + name + '&join=#xplatform-irc';
                     element[0].src = 'http://xplatform.org:8001/#/?autologin=true&host=' + host + '&port=6667&nick=' + name + '&realname=' + name + '&join=#xplatform-irc';
