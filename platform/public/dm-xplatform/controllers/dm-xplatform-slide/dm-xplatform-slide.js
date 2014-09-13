636e8c604a69ea14bce78c917eefd28ba8660f8c
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-09-13 08:07:17 +0200

    quakenet irc

--- platform/public/dm-xplatform/controllers/dm-xplatform-slide/dm-xplatform-slide.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-slide/dm-xplatform-slide.js
@@ -23,7 +23,7 @@ define(['angular',
                     }
 
                     var name = encodeURI(scope.name.replace(/[^0-9a-zA-Z]/g, '_'));
-                    var host = 'Burstfire.UK.EU.GameSurge.net';
+                    var host = 'irc.quakenet.org';
                     //element[0].src = 'http://localhost:8001/#/?autologin=true&host=irc.freenode.org&port=6667&nick=' + name + '&realname=' + name + '&join=#xplatform-irc';
                     element[0].src = 'http://xplatform.org:8001/#/?autologin=true&host=' + host + '&port=6667&nick=' + name + '&realname=' + name + '&join=#xplatform-irc';
                 });
