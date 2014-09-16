6da26fbcf79f264e7e97c2fa27eacee5ec114106
Tomusdrw <tomusdrw@gmail.com>
2014-09-16 13:59:04 +0200

    Gravatar fix.

--- platform/public/dm-gravatar/dm-gravatar.js
+++ platform/public/dm-gravatar/dm-gravatar.js
@@ -1,6 +1,7 @@
 'use strict'
 angular.module('dm-gravatar', []).filter('dmGravatar', [function () {
     return function (input, size) {
+        input = input.replace('http:', 'https:');
         return input ? input + '?s=' + size + '&d=monsterid' : '';
     };
 }]);
