ec9503592757207813a3403532a98bf458afb8be
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-09-16 10:31:29 +0200

    done modal

--- platform/public/dm-xplatform/controllers/dm-xplatform-chapter/dm-xplatform-chapter.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-chapter/dm-xplatform-chapter.js
@@ -62,108 +62,16 @@ define(['angular',
                 //$scope.state.startSecond = $scope.chapter.videodata.timestamp  + $scope.state.length - 5;
             };
 
-
-            $scope.state.onSaveFile = function () {
-                
-                $scope.modalData.saveTitle = $scope.chapter.title;
-                var modalInstance = $modal.open({
-                    templateUrl: '/static/dm-xplatform/controllers/dm-xplatform-chapter/dm-xplatform-chapter-save.html',
-                    controller: 'dmXplatformChapterSave',
-                    size: 'sm',
-                    resolve: {
-                        modalData: function () {
-                            return $scope.modalData;      
-                       }
-                    }
-                });
-
-                modalInstance.result.then(function (save) {
-                    if (!save) {
-                        return;
-                    }
-
-                    var fileToSave = {
-                        title: $scope.modalData.saveTitle,
-                        trainingId: trainingId,
-                        userId: $scope.userId,
-                        chapter: chapterIndex,
-                        second: $scope.state.currentSecond,
-                        slide: $scope.recordingPlayer.slide,
-                        date: new Date()
-                    };
-                    
-                    $http.post('/api/player', fileToSave);
-                    $scope.files.push(fileToSave);
-                });
-
-            };
-
             User.getUserData(function (user) {
                 $scope.userId = user._id;
             });
 
-            $scope.state.onOpenFile = function () {
-                $scope.modalData.openTitle = undefined;
-
-                var modalInstance = $modal.open({
-                    templateUrl: '/static/dm-xplatform/controllers/dm-xplatform-chapter/dm-xplatform-chapter-open.html',
-                    controller: 'dmXplatformChapterOpen',
-                    size: 'sm',
-                    resolve: {
-                        files: function () {
-                            return $scope.files;
-                        },
-                        modalData: function () {
-                            return $scope.modalData;           
-                        }
-                    }
-                });
-
-                modalInstance.result.then(function (open) {
-                    if (!open || $scope.modalData.openTitle === undefined) {
-                        return;
-                    }
-
-                    var file = $scope.modalData.openTitle;
-                    
-                    $scope.state.isPlaying = false;
-                    $scope.state.currentSecond = 0; //reset timer
-                    
-                    $timeout( function () {
-                        $state.go('navbar.player.chapter', {index: file.chapter})
-                        $timeout( function () {
-                            $scope.recordingPlayer.slide = file.slide;
-                            $scope.state.startSecond = file.second;
-                        }, 1000);
-                    }, 500);
-
-                });
-            };
-        
-
-            $scope.state.onRestart = function () {
-                $scope.state.startSecond = $scope.chapter.videodata.timestamp + 1;
-                $timeout (function () {
-                    $scope.state.startSecond = $scope.chapter.videodata.timestamp;
-                }, 500);
-
-            };
-
-            $scope.$watch('state.currentSecond', function (newVal) {
-                if ($scope.state.chapterId !== chapterIndex) {
-                    return;
-                }
-                
-                goToSecond();
-               
-            });
-
             $scope.onEnd = function () {
-                //$scope.state.isPlaying = false;
                 var modalInstance = $modal.open({
                     templateUrl: '/static/dm-xplatform/controllers/dm-xplatform-chapter/dm-xplatform-chapter-next.html',
                     controller: 'dmXplatformChapterNext',
                     size: 'sm',
+                    windowClass: 'course-modal',
                     resolve: {
                         title: function () {
                             return angular.copy($scope.chapter.title);    
