2489786ec7f904a8ec3171e7be1278b32398972d
Tomusdrw <tomusdrw@gmail.com>
2014-09-22 15:44:38 +0200

    Slide-quiz start.

---
+++ platform/app/models/quizanswer.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var QuizAnswer = new Schema({
+    quizId: String,
+    timestamp: Date,
+    lastTask: {
+        type: Schema.Types.ObjectId,
+        ref: 'slide'
+    },
+    answers: [{
+        taskId: String,
+        slide: {
+            type: Schema.Types.ObjectId,
+            ref: 'slide'
+        }, 
+        email: String,
+        description: String,
+        content: Schema.Types.Mixed,
+        timestamp: Date
+    }]
+});
+
+module.exports = mongoose.model('quizanswer', QuizAnswer);
+
---
+++ platform/app/plugins/slide-quiz/slide-quiz.js
@@ -0,0 +1,58 @@
+function toAnswer(body) {
+    return {
+        taskId: body.taskId,
+        slide: body.slideId,
+        email: body.email,
+        description: body.description,
+        content: body.content,
+        timestamp: new Date()
+    };
+}
+
+
+exports.initApi = function(prefix, app, authenticated) {
+    app.post(prefix + "quiz", authenticated, function(req, res) {
+        QuizAnswers.create(req.body.quizId, toAnswer(req.body)).then(function(obj){
+            res.send(200, obj._id.toString());
+        }).then(null, function(err) {
+            console.error(err);
+            res.send(400, err);
+        });
+    });
+
+    app.post(prefix + "quiz/:id", authenticated, function(req, res){
+        QuizAnswers.addAnswer(req.params.id, toAnswer(req.body)).then(function(id) {
+            res.send(200, id.toString());
+        }, function(err){
+            console.error(err);
+            res.send(400, err);
+        });
+    });
+};
+
+
+var QuizAnswer = require('../../models/quizanswer.js');
+var QuizAnswers = {
+
+    findById: function(id) {
+        return QuizAnswer.findById(id).exec();
+    },
+    addAnswer: function(id, answer) {
+        return this.findById(id).then(function(quiz) {
+            quiz.answers.push(answer);
+            quiz.lastTask = answer.slide;
+
+            quiz.save();
+            return quiz._id;
+        });
+    },
+    create: function(quizId, answer) {
+        var quizAnswer = {
+            quizId: quizId,
+            timestamp: new Date(),
+            lastTask: answer.slide,
+            answers: [answer]
+        };
+        return QuizAnswer.create(quizAnswer);
+    }
+};
