2ea6d16b374f228cd1d0f340b76b9dd9092e8b5e
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-09-26 14:49:45 +0200

    annotations link opening in new tab

---
+++ platform/public/dm-xplatform/directives/dm-annotation/dm-annotation.js
@@ -0,0 +1,28 @@
+define(['angular', 'xplatform/xplatform-app'], function (angular, xplatformApp) {
+    xplatformApp.directive('dmAnnotation', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                annotation: '=',
+                showIteration: '='
+            },
+            templateUrl: '/static/dm-xplatform/directives/dm-annotation/dm-annotation.html',
+            link: function (scope, element, attrs) {
+                var fixLinks = function () {
+                    $timeout(function () {
+                        var link = element.find('.dm-anno-markdown a');
+                        if (!link.length) {
+                            return;
+                        }
+                        link.attr('target', '_blank');
+                    });
+                };
+                fixLinks();
+
+                scope.$watch('annotation', fixLinks);
+            }
+        }
+    }]);
+});
+
