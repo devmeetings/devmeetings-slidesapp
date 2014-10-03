ae159ac3d76642b27e5b62ed0bba21d459e574cb
Tomusdrw <tomusdrw@gmail.com>
2014-10-03 23:55:02 +0200

    Fixed plugins paths for templateCache.

--- platform/public/dm-slider/utils/ExtractPath.js
+++ platform/public/dm-slider/utils/ExtractPath.js
@@ -1,7 +1,18 @@
 define([], function () {
+	function removeUpperDirs(path) {
+
+		return path.reduce(function(memo, elem) {
+			if (elem === '..') {
+				return memo.slice(0, memo.length - 1);
+			}
+			memo.push(elem);
+			return memo;
+		}, []);
+	}
+
     return function (module) {
         var path = module.uri.split('/'); 
         path.pop();
-        return path.join('/');
+        return removeUpperDirs(path).join('/');
     };
 });
