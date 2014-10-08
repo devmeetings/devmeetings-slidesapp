7d8eb76b28224ad9aab9982709259542da4f3a82
Tomusdrw <tomusdrw@gmail.com>
2014-10-08 12:12:48 +0200

    Detecting directory.

--- resources/sound_converter.sh
+++ resources/sound_converter.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for name in *.ogg; do
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for name in $DIR/*.ogg; do
   mp3Name="${name/.ogg/.mp3}"
   if [ ! -f "$mp3Name" ]; then
     avconv -i "${name}" -ab 48k "${mp3Name}";
