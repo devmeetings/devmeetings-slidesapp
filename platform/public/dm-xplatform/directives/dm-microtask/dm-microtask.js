ffb3510e3694dc9cea876d3cdf5d17ca825ba458
Tomusdrw <tomusdrw@gmail.com>
2014-10-11 01:23:29 +0200

    Opening microtasks in new window.

--- platform/public/dm-xplatform/directives/dm-microtask/dm-microtask.js
+++ platform/public/dm-xplatform/directives/dm-microtask/dm-microtask.js
@@ -8,7 +8,17 @@ define(['angular', 'xplatform/xplatform-app'], function (angular, xplatformApp) 
             }, 
             templateUrl: '/static/dm-xplatform/directives/dm-microtask/dm-microtask.html',
             link: function (scope, element, attr) {
-                
+                var fixLinks = function () {
+                    $timeout(function () {
+                        var link = element.find('.microtask-desc a');
+                        if (!link.length) {
+                            return;
+                        }
+                        link.attr('target', '_blank');
+                    });
+                };
+                fixLinks();
+                scope.$watch('microtask', fixLinks);
             }
         };
     }]);
