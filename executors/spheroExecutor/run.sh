0de23494eb04f2248c32b6bf17a1c6a3b33723dc
Tomusdrw <tomusdrw@gmail.com>
2014-10-17 23:17:36 +0200

    addAll in burgerExecutor.

--- executors/spheroExecutor/run.sh
+++ executors/spheroExecutor/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo hciconfig hci0 up
 sudo rfcomm release hci0
-sudo rfcomm bind hci0 68:86:E7:02:21:37
+#sudo rfcomm bind hci0 68:86:E7:02:21:37
+sudo rfcomm bind hci0 68:86:E7:03:5E:F2
 sudo chmod 666 /dev/rfcomm0
 
