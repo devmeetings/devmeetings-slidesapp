e800f426cc21c722e81d5d50bbde79cf01784e1e
Tomusdrw <tomusdrw@gmail.com>
2014-12-04 12:08:00 +0100

    Fixed dmGravatar filter.

--- platform/public/dm-modules/dm-gravatar/dm-gravatar.js
+++ platform/public/dm-modules/dm-gravatar/dm-gravatar.js
@@ -3,6 +3,9 @@ define(['angular'], function(angular) {
     angular.module('dm-gravatar', []).filter('dmGravatar', [
         function() {
             return function(input, size) {
+                if (!input) {
+                  return 'https://www.gravatar.com/avatar/ee4d1b570eff6ce63c7d97043980a98c?default=monsterid&forcedefault=1';
+                }
                 input = input.replace('http:', 'https:');
                 return input ? input + '?s=' + size + '&d=monsterid' : '';
             };
