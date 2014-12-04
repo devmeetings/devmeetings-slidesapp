e65916c508d8d6e3ad25af67e071dc585bdd7a78
Tomusdrw <tomusdrw@gmail.com>
2014-12-04 14:47:16 +0100

    Merge.

--- platform/public/dm-xplatform/controllers/dm-xplatform-annos/dm-xplatform-edit-annotation/dm-xplatform-edit-annotation.js
+++ platform/public/dm-xplatform/controllers/dm-xplatform-annos/dm-xplatform-edit-annotation/dm-xplatform-edit-annotation.js
@@ -1,40 +1,39 @@
 define(['angular', 'xplatform/xplatform-app'], function(angular, xplatformApp) {
-    xplatformApp.directive('dmXplatformEditAnnotation', [
-        'dmEvents', '$rootScope',
-        function(dmEvents, $rootScope) {
+  xplatformApp.directive('dmXplatformEditAnnotation', [
+    'dmEvents', '$rootScope',
+    function(dmEvents, $rootScope) {
+      return {
+        restrict: 'E',
+        scope: {
+          snippet: '=',
+          eventId: '=',
+          iterationId: '=',
+          materialId: '='
+        },
+        templateUrl: '/static/dm-xplatform/controllers/dm-xplatform-annos/dm-xplatform-edit-annotation/index.html',
+        link: function(scope) {
 
+          scope.save = function(snippet) {
+            var newSnippet = !snippet._id;
 
-            return {
-                restrict: 'E',
-                scope: {
-                    snippet: '=',
-                    eventId: '=',
-                    iterationId: '=',
-                    materialId: '='
-                },
-                templateUrl: '/static/dm-xplatform/controllers/dm-xplatform-annos/dm-xplatform-edit-annotation/index.html',
-                link: function(scope) {
-                    // TODO [ToDr] Refresh annotations?
+            if (newSnippet) {
+              dmEvents.addEventAnnotation(scope.eventId, scope.iterationId, scope.materialId, snippet).then(function(ev) {
+                scope.snippet = {};
+              });
+            } else {
+              dmEvents.editEventAnnotation(scope.eventId, scope.iterationId, scope.materialId, snippet);
+            }
 
-                    scope.save = function(snippet) {
-                        var newSnippet = !snippet._id;
-                        
-                        if (newSnippet) {
-                            dmEvents.addEventAnnotation(scope.eventId, scope.iterationId, scope.materialId, snippet).then(function(ev){
-                                scope.snippet = {};
-                            });
-                        } else {
-                            dmEvents.editEventAnnotation(scope.eventId, scope.iterationId, scope.materialId, snippet);
-                        }
-                        // TODO [ToDr] :(
-                        $rootScope.$broadcast('newAnnotations');
-                    };
+            // TODO [ToDr] :(
+            $rootScope.$broadcast('newAnnotations');
 
-                    scope.delete = function(snippet) {
-                        dmEvents.deleteEventAnnotation(scope.eventId, scope.iterationId, scope.materialId, snippet);
-                    };
-                }
-            };
+          };
+
+          scope.delete = function(snippet) {
+            dmEvents.deleteEventAnnotation(scope.eventId, scope.iterationId, scope.materialId, snippet);
+          };
         }
-    ]);
+      };
+    }
+  ]);
 });
