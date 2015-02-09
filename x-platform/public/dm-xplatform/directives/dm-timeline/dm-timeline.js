3142cf17cbe8002f53fea5ae917c116042aff802
Tomusdrw <tomusdrw@gmail.com>
2015-02-09 09:09:43 +0100

    Displaying progres sbar.

--- x-platform/public/dm-xplatform/directives/dm-timeline/dm-timeline.js
+++ x-platform/public/dm-xplatform/directives/dm-timeline/dm-timeline.js
@@ -1,15 +1,21 @@
 define(['angular', 'xplatform/xplatform-app', 'xplatform/directives/dm-taskicon/dm-taskicon'], function (angular, xplatformApp) {
-    xplatformApp.directive('dmTimeline', [function() {
-        return {
-            restrict: 'E',
-            scope: {
-                length: '=',
-                annotations: '='
-            },
-            templateUrl: '/static/dm-xplatform/directives/dm-timeline/dm-timeline.html',
-            link: function (scope, element) {
-            }
-        };
-    }]);
+  xplatformApp.directive('dmTimeline', [function() {
+    return {
+      restrict: 'E',
+      scope: {
+        second: '=',
+        length: '=',
+        annotations: '='
+      },
+      templateUrl: '/static/dm-xplatform/directives/dm-timeline/dm-timeline.html',
+      link: function (scope, element) {
+
+        scope.$watch('second', function(second) {
+          scope.value = second * 100 / scope.length;
+        });
+
+      }
+    };
+  }]);
 });
 
