9b1ff4d16905d123aa47b01545ad63285be935c2
Tomusdrw <tomusdrw@gmail.com>
2015-02-11 23:31:01 +0100

    Performance tuning panel.

--- x-platform/public/dm-xplatform/controllers/dm-xplatform-space/dm-xplatform-space.js
+++ x-platform/public/dm-xplatform/controllers/dm-xplatform-space/dm-xplatform-space.js
@@ -3,6 +3,7 @@ define(['angular', 'xplatform/xplatform-app', '_',
   'xplatform/controllers/dm-xplatform-upload/dm-xplatform-upload',
   'xplatform/services/dm-questions/dm-questions',
   'xplatform/directives/dm-iframe/dm-iframe',
+  'xplatform/directives/dm-xplatform-performance/dm-xplatform-performance',
   'xplatform/filters/liveLinkUrl',
 ], function(angular, xplatformApp, _) {
   xplatformApp.controller('dmXplatformSpace', ['$scope', '$rootScope', '$timeout', '$state', '$stateParams', '$location', '$http', '$modal', 'dmEvents', 'dmUser', 'dmQuestions', 'dmSlidesaves', 'Fullscreen',
---
+++ x-platform/public/dm-xplatform/directives/dm-xplatform-performance/dm-xplatform-performance.js
@@ -0,0 +1,43 @@
+define(['angular', 'xplatform/xplatform-app'], function (angular, xplatformApp) {
+  'use strict';
+
+  function readPerformanceData(localStorage) {
+    var perf = localStorage.performance;
+    try {
+      return JSON.parse(perf);
+    } catch(e) {
+      return {};
+    }
+  }
+  function savePerformanceData(localStorage, perf) {
+    localStorage.performance = JSON.stringify(perf);
+  }
+
+  xplatformApp.directive('dmXplatformPerformance', ['$window', '$rootScope', function($window, $rootScope) {
+    return {
+      restrict: 'E',
+      scope:{},
+      templateUrl: '/static/dm-xplatform/directives/dm-xplatform-performance/dm-xplatform-performance.html',
+      link: function (scope) {
+        scope.performance = readPerformanceData($window.localStorage);
+        $rootScope.performance = [];
+
+        prop('anims');
+        prop('shadows');
+
+        function prop(name) {
+          scope.$watch('performance.' + name, function(enabled) {
+            var perf = $rootScope.performance;
+            if (enabled) {
+              perf.push(name);
+            } else if (perf.indexOf(name) > -1) {
+              perf.splice(perf.indexOf(name), 1);
+            }
+            savePerformanceData($window.localStorage, scope.performance);
+          });
+        }
+      }
+    };
+  }]);
+});
+
