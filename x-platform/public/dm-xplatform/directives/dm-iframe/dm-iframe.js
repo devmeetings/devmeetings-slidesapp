5ae2c05714dfc31a15f28a5c515ae705bb64522d
Tomusdrw <tomusdrw@gmail.com>
2015-03-11 11:09:56 +0100

    Delayed loading of iframes.

--- x-platform/public/dm-xplatform/directives/dm-iframe/dm-iframe.js
+++ x-platform/public/dm-xplatform/directives/dm-iframe/dm-iframe.js
@@ -1,21 +1,34 @@
 define(['angular', 'xplatform/xplatform-app'], function(angular, xplatformApp) {
   'use strict';
 
-  xplatformApp.directive('dmIframe', [function() {
+  xplatformApp.directive('dmIframe', function($timeout) {
     return {
       restrict: 'A',
-      link: function(scope, element) {
+      link: function(scope, element, attr) {
         element.addClass('dm-iframe');
-        element.addClass('dm-iframe-hidden');
 
         function removeClass() {
-          element.removeClass('dm-iframe-hidden');
+          // Let the page render in background
+          $timeout(function(){
+            element.removeClass('dm-iframe-hidden');
+          }, 1000);
         }
+
+        // Setting iframe src is dealyed.
+        attr.$observe('dmIframe', function(value) {
+          element.addClass('dm-iframe-hidden');
+          attr.$set('src', null);
+
+          $timeout(function() {
+            attr.$set('src', value);
+          }, 5000);
+        });
+
         element[0].addEventListener('load', removeClass);
         scope.$on('$destroy', function() {
           element[0].removeEventListener('load', removeClass);
         });
       }
     };
-  }]);
+  });
 });
