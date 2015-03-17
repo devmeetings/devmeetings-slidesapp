6df239b92b119378a4fd934338ec1b112249a860
Tomusdrw <tomusdrw@gmail.com>
2015-03-17 09:38:45 +0100

    Xplatform management app.

--- tools/raspberrypi-manager/app.js
+++ tools/raspberrypi-manager/app.js
@@ -1,12 +1,82 @@
 var express = require('express');
-var app = express();
+var app = express(),
+  Q = require('q'),
+  bodyParser = require('body-parser');
 
+var IFACE = 'wlan1';
+
+
+var wifi = require('./wifi');
 app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function(req, res) {
-  res.render('index');
+
+  var wifiData = wifi.readWifi();
+
+  wifiData.done(function(data) {
+
+    res.render('index', {
+      wifi: data
+    });
+
+  });
+
+});
+
+app.post('/', function(req, res) {
+
+  var scan = execCommand('iwlist ' + IFACE + ' scan | grep -ie "ssid" | sed s/^\\\\s\\\\+//g');
+  var wifiData = wifi.readWifi();
+
+  Q.all([scan, wifiData]).done(function(data) {
+    res.render('index', {
+      wifi: data[1],
+      networks: data[0]
+    });
+  });
+
+});
+
+app.post('/wifi', function(req, res) {
+
+  var ssid = req.body.ssid;
+  var psk = req.body.psk;
+
+  wifi.writeWifi(ssid, psk)
+    .then(function() {
+      return execCommand('ifdown ' + IFACE + ' && ifup ' + IFACE);
+    })
+    .done(function() {
+      res.redirect('/');
+    });
+
+});
+
+app.post('/reboot', function(req, res) {
+
+  execCommand('reboot', function() {
+    res.send(200);
+  });
 });
 
 app.use(express.static('public'));
 
-app.listen(3000);
+app.listen(9911);
+
+
+var exec = require('child_process').exec;
+
+function execCommand(cmd) {
+  var defer = Q.defer();
+  exec(cmd, function(err, stdout, stderr) {
+    if (err) {
+      defer.reject(stderr);
+    } else {
+      defer.resolve(stdout);
+    }
+  });
+  return defer.promise;
+}
---
+++ tools/raspberrypi-manager/wifi.js
@@ -0,0 +1,51 @@
+var Q = require('q'),
+  fs = require('fs');
+
+var file = '/etc/wpa_supplicant/wpa_supplicant.conf';
+//var file = '../../deployment/ansible/roles/rasbpi/files/wpa_supplicant.conf';
+
+
+var patterns = {
+  ssid: /ssid="(.+)"/,
+  psk: /psk="(.+)"/
+};
+
+function readFile() {
+  return Q.ninvoke(fs, 'readFile', file);
+}
+
+function writeFile(content) {
+  return Q.ninvoke(fs, 'writeFile', file, content);
+}
+
+function readWifi() {
+
+  return readFile().then(function(content) {
+    var c = content.toString();
+    var ssid = c.match(patterns.ssid)[1];
+    var psk = c.match(patterns.psk)[1];
+
+    return {
+      ssid: ssid,
+      psk: psk
+    };
+  });
+
+}
+
+function writeWifi(ssid, psk) {
+  ssid = ssid.replace(/"/g, '');
+  psk = psk.replace('/"/g', '');
+
+  return readFile().then(function(content) {
+    var c = content.toString();
+    c = c.replace(patterns.ssid, 'ssid="' + ssid + '"');
+    c = c.replace(patterns.psk, 'psk="' + psk + '"');
+    return writeFile(c);
+  });
+}
+
+
+
+exports.readWifi = readWifi;
+exports.writeWifi = writeWifi;
