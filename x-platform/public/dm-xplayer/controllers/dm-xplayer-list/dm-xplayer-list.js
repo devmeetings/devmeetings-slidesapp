380ac4259bede796a5b6fca07899fc8f96607918
Tomusdrw <tomusdrw@gmail.com>
2015-03-21 18:50:20 +0100

    Fix for sorting.

--- x-platform/public/dm-xplayer/controllers/dm-xplayer-list/dm-xplayer-list.js
+++ x-platform/public/dm-xplayer/controllers/dm-xplayer-list/dm-xplayer-list.js
@@ -7,9 +7,9 @@ define([
 
   xplayerApp.controller('dmXplayerList', ['$scope', 'dmRecordings', function($scope, dmRecordings) {
     dmRecordings.getList().then(function(recordings) {
-      $scope.groups = _.groupBy(recordings.data, function(rec){
+      $scope.groups = _.values(_.groupBy(recordings.data, function(rec){
         return rec.group;
-      }).sort(function(g1, g2){
+      })).sort(function(g1, g2){
         return g1.recordings[0].date < g2.recordings[0].date;
       });
     });
