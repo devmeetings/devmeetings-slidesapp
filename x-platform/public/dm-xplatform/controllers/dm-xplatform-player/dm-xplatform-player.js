b49c00dc0453bb55f02fb6a90bae1d248a830908
Tomusdrw <tomusdrw@gmail.com>
2015-03-31 15:48:01 +0200

    Fixing old movie player.

--- x-platform/public/dm-xplatform/controllers/dm-xplatform-player/dm-xplatform-player.js
+++ x-platform/public/dm-xplatform/controllers/dm-xplatform-player/dm-xplatform-player.js
@@ -2,56 +2,55 @@ define(['angular', 'xplatform/xplatform-app', '_',
   'xplatform/services/dm-events/dm-events'
 ], function(angular, xplatformApp, _) {
   'use strict';
-  xplatformApp.controller('dmXplatformPlayer', ['$scope', '$stateParams', '$timeout', 'dmEvents', 'dmRecordings', 'dmBrowserTab',
-    function($scope, $stateParams, $timeout, dmEvents, dmRecordings, dmBrowserTab) {
+  xplatformApp.controller('dmXplatformPlayer', function($scope, $stateParams, $timeout, dmEvents, dmRecordings, dmBrowserTab, dmPlayer) {
 
-      $scope.state = dmEvents.getState($stateParams.event, $stateParams.material);
+    $scope.state = dmEvents.getState($stateParams.event, $stateParams.material);
+    dmPlayer.setRecorderSource(null);
 
-      dmEvents.getEvent($stateParams.event, false).then(function(data) {
-        var material = _.find(data.iterations[$stateParams.iteration].materials, function(elem) {
-          return elem._id === $stateParams.material;
-        });
-
-        dmBrowserTab.setTitleAndIcon(material.title + ' - ' + data.title, 'movie')
-          .withBadge(1 + parseInt($stateParams.iteration, 10));
-        return dmRecordings.getRecording(material.material);
-      }).then(function(recording) {
-        $scope.recording = recording;
-
-        function getDeep(obj, path) {
-          if (!path || !obj) {
-            return obj;
-          }
-          var p = path.split('.');
-          return getDeep(obj[p[0]], p.slice(1).join('.'));
-        }
-        recording.slides = recording.slides.filter(function(slide) {
-          var js = getDeep(slide.code, 'workspace.tabs.main|js.content') || '';
-          var html = getDeep(slide.code, 'workspace.tabs.index|html.content') || '';
-          var jsValid = js.indexOf('html>') === -1;
-          var htmlValid = html.indexOf('function') === -1;
-          return jsValid && htmlValid;
-        });
-        $scope.state.max = recording.slides[recording.slides.length - 2].timestamp / 1000;
+    dmEvents.getEvent($stateParams.event, false).then(function(data) {
+      var material = _.find(data.iterations[$stateParams.iteration].materials, function(elem) {
+        return elem._id === $stateParams.material;
       });
 
-      function fetchAnnotations() {
-        dmEvents.getEventAnnotations($stateParams.event, $stateParams.iteration, $stateParams.material).then(function(annotations) {
-          if ($stateParams.withVoice) {
-            return;
-          }
+      dmBrowserTab.setTitleAndIcon(material.title + ' - ' + data.title, 'movie')
+        .withBadge(1 + parseInt($stateParams.iteration, 10));
+      return dmRecordings.getRecording(material.material);
+    }).then(function(recording) {
+      $scope.recording = recording;
 
-          $scope.annotations = annotations.sort(function(a, b) {
-            return a.timestamp - b.timestamp;
-          }).filter(function(anno) {
-            return anno.type !== 'snippet';
-          });
-        });
+      function getDeep(obj, path) {
+        if (!path || !obj) {
+          return obj;
+        }
+        var p = path.split('.');
+        return getDeep(obj[p[0]], p.slice(1).join('.'));
       }
+      recording.slides = recording.slides.filter(function(slide) {
+        var js = getDeep(slide.code, 'workspace.tabs.main|js.content') || '';
+        var html = getDeep(slide.code, 'workspace.tabs.index|html.content') || '';
+        var jsValid = js.indexOf('html>') === -1;
+        var htmlValid = html.indexOf('function') === -1;
+        return jsValid && htmlValid;
+      });
+      $scope.state.max = recording.slides[recording.slides.length - 2].timestamp / 1000;
+    });
 
-      fetchAnnotations();
-      $scope.$on('newAnnotations', fetchAnnotations);
+    function fetchAnnotations() {
+      dmEvents.getEventAnnotations($stateParams.event, $stateParams.iteration, $stateParams.material).then(function(annotations) {
+        if ($stateParams.withVoice) {
+          return;
+        }
 
+        $scope.annotations = annotations.sort(function(a, b) {
+          return a.timestamp - b.timestamp;
+        }).filter(function(anno) {
+          return anno.type !== 'snippet';
+        });
+      });
     }
-  ]);
+
+    fetchAnnotations();
+    $scope.$on('newAnnotations', fetchAnnotations);
+
+  });
 });
