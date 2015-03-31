9145120434b5d6aa86710de7194d7850cda482e8
Tomusdrw <tomusdrw@gmail.com>
2015-03-31 20:29:38 +0200

    Fixing user's avatar issues.

--- x-platform/app/models/user.js
+++ x-platform/app/models/user.js
@@ -1,42 +1,52 @@
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('user', new mongoose.Schema({
-    userId: {
-        type: String, 
-        required: true
-    },
-    name: {
-        type: String,
-        required: true 
-    },
-    email: { 
-        type: String 
-    },
-    password: { 
-        type: String 
-    },
-    type: { 
-        type: String,
-        enum: ['local', 'g+', 'fb', 'github'], 
-        default: 'local' 
-    },
-    verified: {
-        type: Boolean,
-        default: false 
-    },
-    acl: { 
-        type: [String],
-        enum: ['admin:super', 'admin:slides', 'admin:events', 'trainer', 'student'], 
-        default: 'student'
-    },
-    added: {
-        type: Date, 
-        default: Date.now 
-    },
-    bio: {
-        type: String     
-    },
-    avatar: {
-        type: String        
-    }
+var UserModel = mongoose.model('user', new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  type: {
+    type: String,
+    enum: ['local', 'g+', 'fb', 'github'],
+    default: 'local'
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  acl: {
+    type: [String],
+    enum: ['admin:super', 'admin:slides', 'admin:events', 'trainer', 'student'],
+    default: 'student'
+  },
+  added: {
+    type: Date,
+    default: Date.now
+  },
+  bio: {
+    type: String
+  },
+  avatar: {
+    type: String
+  }
 }));
+
+var gravatar = require('gravatar');
+
+UserModel.schema.pre('save', function(next) {
+  var user = this;
+  user.avatar = gravatar.url(user.email);
+  next();
+});
+
+module.exports = UserModel;
