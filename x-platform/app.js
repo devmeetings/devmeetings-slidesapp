3e8a6874ef369ca96366f90e31101945c71c12ec
Tomusdrw <tomusdrw@gmail.com>
2015-04-26 20:51:18 +0200

    Removing sticky sessions.

--- x-platform/app.js
+++ x-platform/app.js
@@ -6,66 +6,62 @@ var express = require('express'),
   config = require('./config/config'),
   logger = require('./config/logging');
 
-var sticky = require('sticky-session');
-sticky(function() {
-  'use strict';
-  mongoose.connect(config.db);
-  var db = mongoose.connection;
-  db.on('error', function() {
-    throw new Error('unable to connect to database at ' + config.db);
+mongoose.connect(config.db);
+var db = mongoose.connection;
+db.on('error', function() {
+  throw new Error('unable to connect to database at ' + config.db);
+});
+
+var plugins = require('./config/plugins');
+plugins.invokePlugins('init', [config]);
+
+var app = express();
+var proxy;
+if (config.meteorProxy) {
+  proxy = require('http-proxy').createProxyServer({
+    target: config.meteorProxy
+  });
+  var proxy2 = require('http-proxy').createProxyServer({
+    target: 'http://localhost:35729/'
+  });
+  logger.info('Configuring proxy.');
+  app.all('/live/*', proxy.web.bind(proxy));
+  app.all('/livereload/*', proxy.web.bind(proxy2));
+}
+
+var router = express.Router();
+var sessionConfig = require('./config/express')(app, config, router);
+
+require('./config/mail')(app);
+require('./config/routes')(router);
+require('./config/passport');
+
+var server = http2.createServer({
+  key: fs.readFileSync('./config/certs/server.key'),
+  cert: fs.readFileSync('./config/certs/server.crt')
+}, app);
+
+if (config.meteorProxy) {
+  server.on('upgrade', function(req) {
+    if (req.url.indexOf('/live') === 0) {
+      return proxy.ws.apply(proxy, arguments);
+    }
+  });
+}
+// Configure socketio after proxy
+var io = socketio(server);
+require('./config/sockets')(io, sessionConfig);
+
+// Configure blocked
+require('./config/blocked')(function(ms) {
+
+  logger.warn('Blocked for ' + ms + 'ms', {
+    blockedFor: ms
   });
 
-  var plugins = require('./config/plugins');
-  plugins.invokePlugins('init', [config]);
+});
 
-  var app = express();
-  var proxy;
-  if (config.meteorProxy) {
-    proxy = require('http-proxy').createProxyServer({
-      target: config.meteorProxy
-    });
-    var proxy2 = require('http-proxy').createProxyServer({
-      target: 'http://localhost:35729/'
-    });
-    logger.info('Configuring proxy.');
-    app.all('/live/*', proxy.web.bind(proxy));
-    app.all('/livereload/*', proxy.web.bind(proxy2));
-  }
 
-  var router = express.Router();
-  var sessionConfig = require('./config/express')(app, config, router);
-
-  require('./config/mail')(app);
-  require('./config/routes')(router);
-  require('./config/passport');
-
-  var server = http2.createServer({
-    key: fs.readFileSync('./config/certs/server.key'),
-    cert: fs.readFileSync('./config/certs/server.crt')
-  }, app);
-
-  if (config.meteorProxy) {
-    server.on('upgrade', function(req) {
-      if (req.url.indexOf('/live') === 0) {
-        return proxy.ws.apply(proxy, arguments);
-      }
-    });
-  }
-  // Configure socketio after proxy
-  var io = socketio(server);
-  require('./config/sockets')(io, sessionConfig);
-
-  // Configure blocked
-  require('./config/blocked')(function(ms) {
-
-    logger.warn('Blocked for ' + ms + 'ms', {
-      blockedFor: ms
-    });
-
-  });
-
-  return server;
-
-}).listen(config.port, function() {
+server.listen(config.port, function() {
   logger.info('Server listening on port:', config.port);
 });
