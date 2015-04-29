2af79cfa37c1f723e62003786cc9d9f511e153db
Tomusdrw <tomusdrw@gmail.com>
2015-04-29 17:05:53 +0200

    Removed setTimeout.

--- x-platform/app/plugins/slide-recorder/slide-recorder.js
+++ x-platform/app/plugins/slide-recorder/slide-recorder.js
@@ -10,75 +10,73 @@ exports.onSocket = function(log, socket) {
   function patchState(data, ack) {
     var workspaceRoom = getRoom(data);
 
-    setTimeout(function() {
+    States.fetchStateForWriting(data._id, socket.request.user).done(function(save) {
+      var patches = data.patches;
 
-      States.fetchStateForWriting(data._id, socket.request.user).done(function(save) {
-        var patches = data.patches;
+      if (save.fresh && data._id && !patches[0].current) {
+        ack(false);
+        return;
+      }
 
-        if (save.fresh && data._id && !patches[0].current) {
-          ack(false);
-          return;
-        }
+      var originalTime = save.originalTimestamp.getTime();
+      if (!originalTime) {
+        save.originalTimestamp = new Date(patches[0].timestamp);
+        originalTime = patches[0].timestamp;
+      }
 
-        var originalTime = save.originalTimestamp.getTime();
-        if (!originalTime) {
-          save.originalTimestamp = new Date(patches[0].timestamp);
-          originalTime = patches[0].timestamp;
-        }
+      save.currentTimestamp = new Date(_.last(patches).timestamp);
 
-        save.currentTimestamp = new Date(_.last(patches).timestamp);
+      if (patches[0].current) {
+        save.original = patches[0].current;
+        save.current = patches[0].current;
+        patches = [];
+      } else {
+        States.applyPatches(save.current, patches);
+      }
 
-        if (patches[0].current) {
-          save.current = patches[0].current;
-          patches = [];
-        } else {
-          States.applyPatches(save.current, patches);
-        }
+      // fix timestamps
+      var last = save.noOfPatches || 0;
+      patches.map(function(patchData, idx) {
+        patchData.id = save._id + '_' + (last + idx);
+        patchData.timestamp = patchData.timestamp - originalTime;
+      });
+      // append Patches
+      save.workspaceId = data.workspaceId;
 
-        // fix timestamps
-        var last = save.noOfPatches || 0;
-        patches.map(function(patchData, idx) {
-          patchData.id = save._id + '_' + (last + idx);
-          patchData.timestamp = patchData.timestamp - originalTime;
-        });
-        // append Patches
-        save.workspaceId = data.workspaceId;
+      // NOTE [ToDr] For performance reasons not every field is being updated!
+      return States.update(save, patches).then(function(save) {
+        return {
+          save: save,
+          patches: patches
+        };
+      }).done(function(d) {
+        // NOTE [ToDr] For performance reasons you won't get full document here!
+        var save = d.save;
+        var id = save._id;
+        var patchNo = save.noOfPatches - 1;
+        var compoundId = id + '_' + patchNo;
 
-        // NOTE [ToDr] For performance reasons not every field is being updated!
-        return States.update(save, patches).then(function(save) {
-          return {
-            save: save,
-            patches: patches
-          };
-        }).done(function(d) {
-          // NOTE [ToDr] For performance reasons you won't get full document here!
-          var save = d.save;
-          var id = save._id;
-          var patchNo = save.noOfPatches - 1;
-          var compoundId = id + '_' + patchNo;
+        ack(true, id, patchNo);
 
-          ack(true, id, patchNo);
+        // Populating cache
+        cache.populate(compoundId, save.current).done();
 
-          // Populating cache
-          cache.populate(compoundId, save.current).done();
-
-          // Broadcast the whole slide
-          var room = socket.broadcast.to(workspaceRoom);
-          if (data._id !== id.toString()) {
-            room.emit('state.patches', {
-              id: compoundId,
-              current: save.current
-            });
-            return;
-          }
-          // Broadcast new patches to listeners
+        // Broadcast the whole slide
+        var room = socket.broadcast.to(workspaceRoom);
+        if (data._id !== id.toString()) {
           room.emit('state.patches', {
             id: compoundId,
-            patches: d.patches
+            current: save.current
           });
+          return;
+        }
+        // Broadcast new patches to listeners
+        room.emit('state.patches', {
+          id: compoundId,
+          patches: d.patches
         });
       });
-    }, 1000);
+    });
   }
 
 
