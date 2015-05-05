6c8a95c3c040209c0852f66a2cfda2b734e607da
Tomusdrw <tomusdrw@gmail.com>
2015-05-05 12:17:23 +0200

    Better workshoplist.

--- x-platform/public/dm-xplatform/controllers/dm-xplatform-workshoplist/dm-xplatform-workshoplist.js
+++ x-platform/public/dm-xplatform/controllers/dm-xplatform-workshoplist/dm-xplatform-workshoplist.js
@@ -1,4 +1,52 @@
 define(['angular', 'xplatform/xplatform-app', '_'], function(angular, xplatformApp, _) {
+
+  /**
+   * Converts an HSL color value to RGB. Conversion formula
+   * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+   * Assumes h, s, and l are contained in the set [0, 1] and
+   * returns r, g, and b in the set [0, 255].
+   *
+   * @param   Number  h       The hue
+   * @param   Number  s       The saturation
+   * @param   Number  l       The lightness
+   * @return  Array           The RGB representation
+   */
+  function hslToRgb(h, s, l) {
+    var r, g, b;
+
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      var hue2rgb = function hue2rgb(p, q, t) {
+        if (t < 0) {
+          t += 1;
+        }
+        if (t > 1) {
+          t -= 1;
+        }
+        if (t < 1 / 6) {
+          return p + (q - p) * 6 * t;
+        }
+        if (t < 1 / 2) {
+          return q;
+        }
+        if (t < 2 / 3) {
+          return p + (q - p) * (2 / 3 - t) * 6;
+        }
+        return p;
+      };
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  }
+
+
   xplatformApp.controller('dmXplatformWorkshoplist', function($scope, $stateParams, dmEvents, dmUser) {
 
     dmEvents.allEvents().then(function(events) {
@@ -29,6 +77,21 @@ define(['angular', 'xplatform/xplatform-app', '_'], function(angular, xplatformA
           return c._id === event._id;
         });
       });
+    };
+
+    $scope.getBackgroundColor = function(course) {
+      var id = parseInt(course._id, 16) % 360;
+
+      var color = hslToRgb(id / 360, 0.2, 0.3);
+
+      return '#' + color.map(function(c) {
+        return c.toString(16);
+      }).join('');
+    };
+
+    $scope.getDescription = function(courseTitle) {
+      var chars = /([a-z]{4})/i.exec(courseTitle);
+      return chars[1];
     };
 
   });
