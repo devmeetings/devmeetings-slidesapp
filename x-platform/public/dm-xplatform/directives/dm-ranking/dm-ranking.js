1f558540417b3a4d4d22e5af0c6c6f2d6241aa8c
Arnimarl <lukaszslomski13@gmail.com>
2015-08-04 20:27:14 +0200

    Work in progress.

--- x-platform/public/dm-xplatform/directives/dm-ranking/dm-ranking.js
+++ x-platform/public/dm-xplatform/directives/dm-ranking/dm-ranking.js
@@ -40,7 +40,7 @@ export class RankingDir {
       // jak to widze:
       // na back-end zostaje wyslana nazwa nowej grupy i jesli
       // grupa z taka nazwa nie istnieje, back-end
-      // dodaje do listy grup, po czym rozsyla ją do frontu, zeby uzytkownicy
+      // dodaje do listy grup, po czym rozsyla ja do frontu, zeby uzytkownicy
       // mogli przyporzadkowac sie
     };
 
@@ -56,6 +56,7 @@ export class RankingDir {
 
       scope.groups = this.generateGroups(scope.ranking);
       this.populateGroupsWithRankings(scope.ranking, scope.groups);
+      scope.aggregatedGroup = this.aggregateGroup(scope, scope.groups[2], scope.event.iterations);
     });
 
     function findMaxNoOfTasks (iterationIdx) {
@@ -75,6 +76,143 @@ export class RankingDir {
       return _.range(0, Math.max(maxFromEvent, max));
     };
   }
+
+  // generateGroups(ranking) {
+  //   var o = _.groupBy(ranking, 'group');
+  //   // var o = {
+  //   //   'Grupa A' : [{}, {} , {}],
+  //   //   'Grupa B': [{}, {}]
+  //   // }
+
+  //   var keys  = Object.keys(o);
+  //   // keys === ['Grupa A', 'Grupa B'];
+
+  //   return keys.map((groupName) => {
+  //     return {
+  //       name: groupName,
+  //       ranks: o[groupName]
+  //     };
+  //   });
+  // }
+
+  /*
+      [
+   *     {
+   *        data: {
+                "0_0": { isDone: true }
+                "0_1": { isDone: true }
+                "1_0": { isDone: false}
+          }
+   *     },
+   *     {
+   *        data: {
+                "0_0": { isDone: false }
+                "0_1": { isDone: true }
+                "1_0": { isDone: false }
+          }
+   *     }
+   *   ]
+  */
+  aggregateResultsForIteration (scope, ranks, iterationIdx) {
+    let results = [];
+
+    let tasksInIteration = scope.getTasks(ranks[0], iterationIdx);
+
+    for (let task of tasksInIteration) {
+      // let taskIdx = tasksInIteration[task];
+      let id = iterationIdx + '_' + task;
+
+      let sum = 0;
+      for (let rank of ranks) {
+
+        if (rank.data[id] && rank.data[id].isDone) {
+          sum += 1;
+        }
+      }
+      results.push(sum);
+    }
+
+    return results;
+
+      /*
+      return [
+        1 + 0,
+        1 + 1
+      ];*/
+  }
+  /*
+   * group = {
+   *   name: 'Grupa A',
+   *   ranks: [
+   *     {
+   *        data: {
+                "0_0": { isDone: true }
+                "0_1": { isDone: true }
+                "1_0": { isDone: false}
+          }
+   *     },
+   *     {
+   *        data: {
+                "0_0": { isDone: false }
+                "0_1": { isDone: true }
+                "1_0": { isDone: false}
+          }
+   *     }
+   *   ]
+   * }
+   * eventIteration = [,,]
+   */
+  aggregateGroup (scope, group, eventIterations) {
+    let results = [];
+
+    for (let i = 0; i < eventIterations.length; i++) {
+      let resultsForIteration = this.aggregateResultsForIteration(scope, group.ranks, i);
+      results.push(resultsForIteration);
+    }
+
+    return {
+      name: group.name,
+      noOfUsers: group.ranks.length,
+      results: results
+    };
+
+    //   [
+    //     //iteracja0
+    //     [
+    //       //ile osob zrobilo zadanie 0_0
+    //       1 + 0
+    //       //ile osob zrobilo zadanie 0_1
+    //       1 + 1
+    //     ],
+    //     //iteracja1
+    //     [
+    //       //ile osob zrobilo zadanie 1_0
+    //       0 + 0
+    //     ]
+    //   ]
+    // };
+  }
+
+  // aggregateGroupImpl(group) {
+
+  //   return {
+  //     noOfUsers: group.ranks.length,
+  //     results: [
+  //       //iteracja0
+  //       [
+  //         //ile osob zrobilo zadanie 0_0
+  //         1 + 0
+  //         //ile osob zrobilo zadanie 0_1
+  //         1 + 1
+  //       ],
+  //       //iteracja1
+  //       [
+  //         //ile osob zrobilo zadanie 1_0
+  //         0 + 0
+  //       ]
+  //     ]
+  //   };
+  // }
 
   generateGroups (ranking) {
     let groups = [];
