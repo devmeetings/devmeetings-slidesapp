7918ff313c6c4c56328d53dd6e10b92966693860
Arnimarl <lukaszslomski13@gmail.com>
2015-08-21 01:59:26 +0200

    Preparred to communication with back-end.

--- x-platform/public/dm-xplatform/directives/dm-ranking/dm-ranking.js
+++ x-platform/public/dm-xplatform/directives/dm-ranking/dm-ranking.js
@@ -6,7 +6,7 @@ import xplatformApp from 'dm-xplatform/xplatform-app';
 import _ from '_';
 import viewTemplate from './dm-ranking.html!text';
 
-export class RankingDir {
+class Ranking {
 
   constructor (data) {
     _.extend(this, data);
@@ -15,92 +15,119 @@ export class RankingDir {
   link (scope) {
     this.dmUser.getCurrentUser().then((user) => {
       scope.currentUser = user.result;
-      console.log('scope.currentUser:');
-      console.log(scope.currentUser);
     });
-
-    scope.userJoinToGroup = function (groupName) {
-      let currentUserRank = _.find(scope.ranking, function (ranking) {
-        return ranking.user._id === scope.currentUser._id;
-      });
-      currentUserRank.group = groupName;
-      console.log(currentUserRank);
-
-      // ranking uzytkownika ze zmieniana grupa leci na back-end, ktory
-      // dokonuje wpisu w DB, nastepnie info o dokonanej zmianie leci do frontu(ow)
-    };
-
-    scope.createNewGroup = (newGroupName) => {
-
-      // To jednak chyba powinno byc po back-endzie
-      if (this.groupAlreadyExist(scope.groups, newGroupName) || !newGroupName) {
-        return;
-      }
-      console.log('New group was created: ', newGroupName);
-      // jak to widze:
-      // na back-end zostaje wyslana nazwa nowej grupy i jesli
-      // grupa z taka nazwa nie istnieje, back-end
-      // dodaje do listy grup, po czym rozsyla ja do frontu, zeby uzytkownicy
-      // mogli przyporzadkowac sie
-    };
-
+    scope.trainerAcl = 2;
     this.dmRanking.getCurrentRanking();
     scope.rankingService = this.dmRanking;
-    scope.rankingViews = ['Ranking (Users)', 'Ranking (Groups - aggregated)'];
+    scope.rankingViews = ['Users Ranking', 'Groups Ranking'];
+    scope.alerts = [];
+    scope.model = {
+      currentRankingView: scope.rankingViews[1],
+      showUsersDetailsinGroup: [],
+      representateAsChecks: true
+    };
 
     scope.$watch('rankingService.currentRanking', (currentRanking) => {
+      // sugestia - zeby watch nie odpalal czesciej niz np. co 5-10s
       // Transform ranking to array
       scope.ranking = _.reduce(currentRanking, (memo, value) => {
         memo.push(value);
         return memo;
       }, []);
-
+      scope.ranking = this.countUserResults(scope.ranking);
       scope.groups = this.generateGroups(scope.ranking);
       this.populateGroupsWithRankings(scope.ranking, scope.groups);
       scope.aggregatedGroups = this.getArrWithAgreggatedGroups(scope, scope.groups, scope.event.iterations);
+      scope.currentUser.group = this.getCurrentUserGroup(scope.currentUser, scope.ranking);
+      scope.theBestGroup = this.getTheBestGroup(scope.aggregatedGroups);
+      scope.getTheBestUserRank = this.getTheBestUserRank(scope.ranking);
     });
 
-    function findMaxNoOfTasks (iterationIdx) {
-      let it = scope.event.iterations[iterationIdx];
-      return it.tasks.reduce((memo, task) => {
-        return memo + (task.noOfTasks || 0);
-      }, 0);
-    }
+    scope.getTasks = (item, iterationIdx) => {
 
-    scope.getTasks = function (item, iterationIdx) {
       if (!item.counts) {
         return [];
       }
       let max = item.counts[iterationIdx] || 0;
-      let maxFromEvent = findMaxNoOfTasks(iterationIdx);
+      let maxFromEvent = this.findMaxNoOfTasks(scope.event.iterations, iterationIdx);
 
       return _.range(0, Math.max(maxFromEvent, max));
     };
+
+    scope.properForTooltip = function (doneBy) {
+
+      if (!(scope.currentUser.acl.length >= scope.trainerAcl)) {
+        return;
+      }
+      let users = '';
+      for (let user of doneBy) {
+        users = users + '\n' + user;
+      }
+      return users;
+    };
+
+    scope.userJoinToGroup = (groupName) => {
+      let currentUserRank = this.getCurrentUserRank(scope.ranking, scope.currentUser._id);
+      currentUserRank.group = groupName;
+      // TODO BACK-END
+      //
+      // ranking uzytkownika ze zmieniana grupa leci na back-end, ktory
+      // dokonuje wpisu w DB, nastepnie info o dokonanej zmianie leci do frontu
+    };
+
+    scope.createNewGroup = (newGroupName) => {
+
+      if (!newGroupName) {
+        scope.alerts.push({type: 'warrning', msg: 'No idea how to name group? Be creative!'});
+        return;
+      }
+      if (this.groupAlreadyExist(scope.groups, newGroupName)) {
+        scope.alerts.push({type: 'warrning', msg: 'Group with this name already exist!'});
+        return;
+      }
+
+      let currentUserRank = this.getCurrentUserRank(scope.ranking, scope.currentUser._id);
+      currentUserRank.group = newGroupName;
+      // TODO BACK-END
+      //
+      // w skrocie:
+      // uzytkownik tworzy grupe automatycznie dodajac sie do niej po zatwierdzeniu nazwy
+      //
+      // rozwiniecie:
+      // ranking uzytkownika z nowa grupa leci na back-end, ktory
+      // dokonuje wpisu w DB, nastepnie info o dokonanej zmianie leci do frontu
+      // (informacje o tym ze grupa istnieje sa przechowywane w rankingu eventu z rankami userow, wiec
+      // na tej podstawie po froncie bedziemy miec nowa grupe, do ktorej inni userzy beda mogli sie dodac)
+    };
+
+    scope.closeAlert = function (index) {
+      scope.alerts.splice(index, 1);
+    };
+
   }
 
-  rankExist (group, item) {
-    var rankFound = _.find(group.ranks, {
-      _id: item._id
-    });
-
-    return !!rankFound;
+  findMaxNoOfTasks (eventIterations, iterationIdx) {
+    let it = eventIterations[iterationIdx];
+    return it.tasks.reduce((memo, task) => {
+      return memo + (task.noOfTasks || 0);
+    }, 0);
   }
 
   generateGroups (ranking) {
     let groups = [];
 
     for (let item of ranking) {
-      let group = {
-        name: item.group,
-        ranks: []
-      };
+      if (item.group) {
+        let group = {
+          name: item.group,
+          ranks: []
+        };
 
-      if (!this.groupAlreadyExist(groups, group.name)) {
-        groups.push(group);
+        if (!this.groupAlreadyExist(groups, group.name)) {
+          groups.push(group);
+        }
       }
     }
-
-    console.log(groups);
     return groups;
   }
 
@@ -108,20 +135,15 @@ export class RankingDir {
 
     _.map(ranking, (item) => {
       _.map(groups, (group) => {
-        if (group.name === item.group) {
-          if (!group.ranks.length) {
-            group.ranks.push(item);
-          }else {
-            if (!(this.rankExist(group, item))) {
-              group.ranks.push(item);
-            }
-          }
+        if (item.group && item.group === group.name) {
+          group.ranks.push(item);
         }
       });
     });
   }
 
   groupAlreadyExist (groups, newGroupName) {
+
     for (let group of groups) {
       if (group.name === newGroupName) {
         return true;
@@ -132,47 +154,119 @@ export class RankingDir {
 
   aggregateResultsForIteration (scope, ranks, iterationIdx) {
     let results = [];
-
     let tasksInIteration = scope.getTasks(ranks[0], iterationIdx);
+    let allGainedPoints = 0;
+    let maxNumOfPointsToGain = 0;
 
     for (let task of tasksInIteration) {
       let id = iterationIdx + '_' + task;
-
       let sum = 0;
+      let doneBy = [];
+
       for (let rank of ranks) {
 
         if (rank.data[id] && rank.data[id].isDone) {
           sum += 1;
+          allGainedPoints += 1;
+          doneBy.push(rank.user.name);
         }
+        maxNumOfPointsToGain += 1;
       }
-      results.push(sum);
+
+      results.push({
+        result: sum,
+        doneBy: doneBy
+      });
     }
 
-    return results;
+    return {
+      results: results,
+      allGainedPoints: allGainedPoints,
+      maxNumOfPointsToGain: maxNumOfPointsToGain
+    };
   }
 
   aggregateGroup (scope, group, eventIterations) {
     let results = [];
+    let users = [];
+    let allGainedPoints = 0;
+    let maxNumOfPointsToGain = 0;
 
     for (let i = 0; i < eventIterations.length; i++) {
-      let resultsForIteration = this.aggregateResultsForIteration(scope, group.ranks, i);
+      let result = this.aggregateResultsForIteration(scope, group.ranks, i);
+      let resultsForIteration = result.results;
+      allGainedPoints += result.allGainedPoints;
+      maxNumOfPointsToGain += result.maxNumOfPointsToGain;
       results.push(resultsForIteration);
     }
+
+    for (let rank of group.ranks) {
+      users.push(rank.user);
+    }
+
+    let resultSummary = {
+      allGainedPoints: allGainedPoints,
+      maxNumOfPointsToGain: maxNumOfPointsToGain,
+      percentageResult: Math.round((allGainedPoints / maxNumOfPointsToGain) * 10000) / 100
+    };
 
     return {
       name: group.name,
       noOfUsers: group.ranks.length,
-      results: results
+      users: users,
+      results: results,
+      resultSummary: resultSummary
     };
   }
 
-  getArrWithAgreggatedGroups (scope, groups, eventIiterations) {
+  getArrWithAgreggatedGroups (scope, groups, eventIterations) {
     let aggregatedGroups = [];
-    for (let group of scope.groups) {
-      let aggregatedGroup = this.aggregateGroup(scope, group, eventIiterations);
+
+    for (let group of groups) {
+      let aggregatedGroup = this.aggregateGroup(scope, group, eventIterations);
       aggregatedGroups.push(aggregatedGroup);
     }
     return aggregatedGroups;
+  }
+
+  getCurrentUserGroup (currentUser, ranking) {
+
+    for (let rank of ranking) {
+      if (currentUser._id === rank.user._id) {
+        return rank.group;
+      }
+    }
+  }
+
+  countUserResults (ranking) {
+
+    for (let rank of ranking) {
+      let result = 0;
+      _.forIn(rank.data, function (value, key) {
+        if (value.isDone) {
+          result += 1;
+        }
+      });
+      rank.result = result;
+    }
+    return ranking;
+  }
+
+  getTheBestGroup (groups) {
+    return _.max(groups, function (group) {
+      return group.resultSummary.percentageResult;
+    });
+  }
+  getTheBestUserRank (ranking) {
+    return _.max(ranking, function (rank) {
+      return rank.result;
+    });
+  }
+
+  getCurrentUserRank (ranking, currentUserId) {
+    return _.find(ranking, function (ranking) {
+      return ranking.user._id === currentUserId;
+    });
   }
 
 }
@@ -187,10 +281,11 @@ xplatformApp.directive('dmRanking', (dmRanking, dmUser) => {
     },
     template: viewTemplate,
     link: (scope) => {
-      let contextMenu = new RankingDir({
+      let contextMenu = new Ranking({
       dmRanking, dmUser});
       contextMenu.link(scope);
     }
   };
 
 });
+
