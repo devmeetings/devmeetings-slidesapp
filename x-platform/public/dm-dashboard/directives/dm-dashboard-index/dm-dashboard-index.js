0027037fd0ea7f229936b17dcff1ef676ada9faf
Arnimarl <lukaszslomski13@gmail.com>
2015-09-03 18:17:57 +0200

    Work in progress.

--- x-platform/public/dm-dashboard/directives/dm-dashboard-index/dm-dashboard-index.js
+++ x-platform/public/dm-dashboard/directives/dm-dashboard-index/dm-dashboard-index.js
@@ -273,19 +273,23 @@ class DmDashboardIndex {
         reportedProblems: [
           {
             title: 'Brak Internetu',
-            description: 'Nie mamy dostepu do Internetu'
+            description: 'Nie mamy dostepu do Internetu',
+            solved: false
           },
           {
             title: 'Brak wody',
-            description: 'Brak wody, nie mozna sie napic, umieramy z pragnienia'
+            description: 'Brak wody, nie mozna sie napic, umieramy z pragnienia',
+            solved: false
           },
           {
             title: 'Mentor Chory',
-            description: 'Jeden Mentor - Kuba Xiniski jest chory i nie przyjechal'
+            description: 'Jeden Mentor - Kuba Xiniski jest chory i nie przyjechal',
+            solved: true
           },
           {
             title: 'Lorem Ipsum dolor',
-            description: 'Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker.'
+            description: 'Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker.',
+            solved: true
           }
         ],
         ranking: {
@@ -383,15 +387,18 @@ class DmDashboardIndex {
         reportedProblems: [
           {
             title: 'Brak Internetu',
-            description: 'Nie mamy dostepu do Internetu'
+            description: 'Nie mamy dostepu do Internetu',
+            solved: false
           },
           {
             title: 'Brak wody',
-            description: 'Brak wody, nie mozna sie napic, umieramy z pragnienia'
+            description: 'Brak wody, nie mozna sie napic, umieramy z pragnienia',
+            solved: false
           },
           {
             title: 'Mentor Chory',
-            description: 'Jeden Mentor - Kuba Xiniski jest chory i nie przyjechal'
+            description: 'Jeden Mentor - Kuba Xiniski jest chory i nie przyjechal',
+            solved: false
           }
         ],
         ranking: {
@@ -507,15 +514,145 @@ class DmDashboardIndex {
         reportedProblems: [
           {
             title: 'Brak Internetu',
-            description: 'Nie mamy dostepu do Internetu'
+            description: 'Nie mamy dostepu do Internetu',
+            solved: false
           },
           {
             title: 'Brak wody',
-            description: 'Brak wody, nie mozna sie napic, umieramy z pragnienia'
+            description: 'Brak wody, nie mozna sie napic, umieramy z pragnienia',
+            solved: false
           },
           {
             title: 'Mentor Chory',
-            description: 'Jeden Mentor - Kuba Xiniski jest chory i nie przyjechal'
+            description: 'Jeden Mentor - Kuba Xiniski jest chory i nie przyjechal',
+            solved: false
+          }
+        ],
+        ranking: {
+          bestUser: {
+            _id: '123',
+            name: 'Kuba Rudnicki',
+            avatar: '',
+            percentageResult: 50.3
+          },
+          bestGroup: {
+            _id: '321',
+            name: 'Ninja JS!',
+            percentageResult: 43.7
+          }
+        },
+        hyperlinks: {
+          rankingView: 'https://www.google.com',
+          trainerView: 'https://www.google.com'
+        },
+        streaming: {
+          active: false,
+          url: 'https://www.youtube.com/embed/1G4isv_Fylg?list=RD1G4isv_Fylg'
+        }
+      },
+      // Krakow
+      {
+        _id: '12345xxx',
+        name: 'RWD Krakow',
+        timing: {
+          started: true,
+          startedAt: 8.55,
+          expectedEnd: 15.55
+        },
+        members: {
+          number: 35,
+          organizers: [
+            {
+              name: 'Tomasz Drwięga',
+              avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+            },
+            {
+              name: 'Łukasz Słomski',
+              avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+            }
+          ],
+          // na razie nie uzywam tej tablicy(trainers), takie rozdrabnianie podzialu moze byc potem
+          // klopotliwe, a nie wiem, czy w ogole potrzebne
+          // trainers: [],
+          students: {
+            all: [
+              {
+                name: 'Kuba Bartczuk',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Alek Cieślak',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Bruno Gałka',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Ewelina Opałka',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Sławek Śmietanka',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Małgosia Kordus',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Kasia Jankowska',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Tomek Szczypiński',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Kuba Rudnicki',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Paweł Biczysko',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              }
+            ],
+            active: [
+              {
+                name: 'Kuba Bartczuk',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Kasia Jankowska',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Kuba Rudnicki',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              },
+              {
+                name: 'Alek Cieślak',
+                avatar: 'https://www.gravatar.com/avatar/1dc4a7d97cb29cf81d0c58bc1a17a5af'
+              }
+            ]
+          }
+        },
+        currentStage: 'Iteration 2',
+        reportedProblems: [
+          {
+            title: 'Brak Internetu',
+            description: 'Nie mamy dostepu do Internetu',
+            solved: false
+          },
+          {
+            title: 'Brak wody',
+            description: 'Brak wody, nie mozna sie napic, umieramy z pragnienia',
+            solved: false
+          },
+          {
+            title: 'Mentor Chory',
+            description: 'Jeden Mentor - Kuba Xiniski jest chory i nie przyjechal',
+            solved: false
           }
         ],
         ranking: {
@@ -563,11 +700,7 @@ class DmDashboardIndex {
     let allProblems = 0;
 
     for (let event of events) {
-      for (let problem of event.reportedProblems) {
-        if (!problem.solved) {
-          allProblems++;
-        }
-      }
+      allProblems = allProblems + this.getNumOfUnsolvedProblems(event.reportedProblems);
     }
     return allProblems;
   }
