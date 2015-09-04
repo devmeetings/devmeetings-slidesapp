913870e768443de938d51e79cb58b12c10dcb8de
Tomusdrw <tomusdrw@gmail.com>
2015-09-04 22:05:15 +0200

    Option to use local.xplatform for some resources.

--- x-platform/public/dm-xplatform/xplatform-app.js
+++ x-platform/public/dm-xplatform/xplatform-app.js
@@ -20,7 +20,8 @@ define(['angular',
   'dm-modules/dm-intro/dm-intro',
   'dm-modules/dm-sref-show/dm-sref-show',
   'dm-modules/dm-online/dm-online',
-  'dm-modules/dm-history/dm-history'
+  'dm-modules/dm-history/dm-history',
+  'dm-modules/dm-local-data/dm-local-data'
 ], function (angular) {
   'use strict';
 
@@ -29,6 +30,7 @@ define(['angular',
     'ui.router', 'ui.bootstrap',
     'angularMoment', 'cfp.hotkeys', 'hc.marked', 'FBAngular', 'ngAnimate', 'ngScrollbar',
     'dm-xplayer', 'dm-user', 'dm-mongotime', 'dm-gravatar',
-    'dm-wavesurfer', 'dm-browsertab', 'dm-recorder', 'dm-history', 'dm-online', 'dm-intro', 'dm-sref-show'
+    'dm-wavesurfer', 'dm-browsertab', 'dm-recorder', 'dm-history', 'dm-online', 'dm-intro', 'dm-sref-show',
+    'dm-local-data'
   ]);
 });
