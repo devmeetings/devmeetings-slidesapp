87840b0cb347599db5a1b1adfc15b9e0c8edad60
Tomusdrw <tomusdrw@gmail.com>
2015-09-15 18:14:09 +0200

    Replacing levelDb and mogno cache with in-memory cache

--- x-platform/app/services/cache.js
+++ x-platform/app/services/cache.js
@@ -1,77 +1,50 @@
-var Cache = require('../models/cache');
-var level = require('level');
-var cap = require('level-capped');
 var Q = require('q');
 
-var instance = process.env.NODE_APP_INSTANCE || 0;
-var databaseLocation = __dirname + '/../../data/' + instance + '/';
-var db = level(databaseLocation);
+var cache = {
+  map: {},
+  keys: [],
+  cap: 5000,
 
-cap(db, 'cache', 5000);
+  put: function (key, value) {
+    this.map[key] = value;
+    this.keys.push(key);
+    this.cleanup();
+  },
 
-function findCacheInMongo (key) {
-  'use strict';
-  return Q.when(Cache.findOne({
-    key: key
-  }).exec());
-}
+  get: function (key) {
+    return this.map[key];
+  },
 
-function putToCacheInMongo (key, value) {
-  'use strict';
-  // [ToDr] Using upsert here to gently overcome possible race conditions.
-  return Q.when(Cache.update({
-    key: key
-  }, {
-    $setOnInsert: {
-      key: key,
-      createdAt: new Date(),
-      content: value
+  cleanup: function () {
+    while (this.keys.length > this.cap) {
+      var key = this.keys.shift();
+      delete this.map[key];
     }
-  }, {
-    upsert: true
-  }));
-}
-
-function findCacheInLevel (key) {
-  'use strict';
-  return Q.ninvoke(db, 'get', 'cache!' + key).then(function (data) {
-    return JSON.parse(data);
-  });
-}
-
-function putToCacheInLevel (key, value) {
-  'use strict';
-  return Q.ninvoke(db, 'put', 'cache!' + key, JSON.stringify({
-    key: key,
-    date: new Date(),
-    content: value
-  }));
-}
+  }
+};
 
 module.exports = {
   get: function (key, generatingFunction) {
     'use strict';
 
     var that = this;
+    var doc = cache.get(key);
 
-    return Q.any([findCacheInMongo(key), findCacheInLevel(key)]).then(function (doc) {
-      if (doc) {
-        return doc.content;
-      }
+    if (doc) {
+      return Q.when(doc.content);
+    }
 
-      // Populate cache
-      return Q.when(generatingFunction(key)).then(function (doc) {
-        return that.populate(key, doc).then(function () {
-          return doc;
-        });
+    // Populate cache
+    return Q.when(generatingFunction(key)).then(function (doc) {
+      return that.populate(key, doc).then(function () {
+        return doc;
       });
     });
   },
 
   populate: function (key, value) {
     'use strict';
-
-    return Q.any([putToCacheInMongo(key, value), putToCacheInLevel(key, value)]);
+    return Q.when(cache.put(key, value));
   }
 
 };
