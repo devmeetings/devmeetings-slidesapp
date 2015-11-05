5a5fd2d0f6ee273c53e715515417a83d371fc460
Arnimarl <lukaszslomski13@gmail.com>
2015-11-05 14:46:16 +0100

    Lint error fixed.

--- x-platform/public/dm-dashboard/directives/dm-dashboard-tiles/dm-dashboard-tiles.js
+++ x-platform/public/dm-dashboard/directives/dm-dashboard-tiles/dm-dashboard-tiles.js
@@ -34,6 +34,8 @@ class DmDashboardTiles {
   getAllEventsNum (vm) {
     let allEventsNum = 0;
     for (let event of vm.dashboard.activeEvents) {
+      // because of lint error - I must use event even if I won't
+      event;
       allEventsNum++;
     }
     return allEventsNum;
