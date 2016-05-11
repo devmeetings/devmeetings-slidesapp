278cafd31e4613c7e6f5b714cec116e333b7b7ec
Tomusdrw <tomusdrw@gmail.com>
2016-05-11 23:29:23 +0200

    Adding some messages

--- executors/webpackExecutor/index.js
+++ executors/webpackExecutor/index.js
@@ -2,9 +2,12 @@
 
 var exec = require('../fsExecutor/executor.js');
 exec('exec_webpack', [
+  ['echo', 'Restoring cached modules'],
   ['cp', '-r', __dirname + '/common/node_modules', '.'],
   ['cp', __dirname + '/common/cpfiles.sh', '.'],
+  ['echo', 'Installing dependencies'],
   ['npm', 'i', '--no-progress'],
+  ['echo', 'Building Webpack'],
   ['./node_modules/.bin/webpack'],
   ['./cpfiles.sh']
 ], {
